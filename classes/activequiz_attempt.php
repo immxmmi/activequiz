bc1b5423d9838c4a15085fc6f8098c87331ced13
immxmmi <immxmmi@gmx.at>
2022-05-15 16:01:14 +0200

    test

--- classes/activequiz_attempt.php
+++ classes/activequiz_attempt.php
@@ -371,7 +371,7 @@ class activequiz_attempt {
      * @return array
      */
     public function get_questions() {
-        echo$this->questionmanager->get_questions()." _____________________/n";
+        echo$this->questionmanager->get_questions()[0];
         return $this->questionmanager->get_questions();
     }
 
