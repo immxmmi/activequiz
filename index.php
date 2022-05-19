8c7e2a54e68b6722ccd30cfd90821f459e996e5a
immxmmi <immxmmi@gmx.at>
2022-05-19 11:20:21 +0200

    test

--- index.php
+++ index.php
@@ -40,81 +40,80 @@
 require_once("../../config.php");
 require_once("lib.php");
 
-$id = required_param('id', PARAM_INT);   // course
-
-if (!$course = $DB->get_record('course', array('id' => $id))) {
-    error("Course ID is incorrect");
-}
-
-// Wenn man auf Quiz drückt startet kommt man auf die Start Seite
-$PAGE->set_url(new moodle_url('/mod/activequiz/index.php', array('id' => $course->id)));
-require_course_login($course);
-$PAGE->set_pagelayout('incourse');
-
-
-
-/// Get all required strings
-$stractivequizzes = get_string("modulenameplural", "activequiz");
-$stractivequiz = get_string("modulename", "activequiz");
-
-// Navigation wird hinzugefügt
-$PAGE->navbar->add($stractivequizzes);
-// Titel vom Quiz wird hinzugefügt
-$PAGE->set_title(strip_tags($course->shortname . ': ' . $stractivequizzes));
-
-
-//$PAGE->set_heading($course->fullname);
-echo $OUTPUT->header();
-
-/// Get all the appropriate data
-echo    get_all_instances_in_course("activequiz", $course);
-
-if (!$activequizs = get_all_instances_in_course("activequiz", $course)) {
-    notice("There are no activequizes", "../../course/view.php?id=$course->id");
-    die;
-}
-
-/// Print the list of instances (your module will probably extend this)
-
-$timenow = time();
-$strname = get_string("name");
-$strweek = get_string("week");
-$strtopic = get_string("topic");
-
-
-$table = new html_table();
-
-if ($course->format == "weeks") {
-    $table->head = array($strweek, $strname);
-    $table->align = array("center", "left");
-} else if ($course->format == "topics") {
-    $table->head = array($strtopic, $strname);
-    $table->align = array("center", "left");
-} else {
-    $table->head = array($strname);
-    $table->align = array("left", "left");
-}
-
-foreach ($activequizs as $activequiz) {
-    $url = new moodle_url('/mod/activequiz/view.php', array('id' => $activequiz->coursemodule));
-    if (!$activequiz->visible) {
-        //Show dimmed if the mod is hidden
-        $link = '<a class="dimmed" href="' . $url . '">' . $activequiz->name . '</a>';
-    } else {
-        //Show normal if the mod is visible
-        $link = '<a href="' . $url . '">' . $activequiz->name . '</a>';
-    }
-
-    if ($course->format == 'weeks' or $course->format == 'topics') {
-        $table->data[] = array($activequiz->section, $link);
-    } else {
-        $table->data[] = array($link);
-    }
-}
-
-echo html_writer::table($table);
-
-/// Finish the page
-
-echo $OUTPUT->footer();
+//$id = required_param('id', PARAM_INT);   // course
+//
+//if (!$course = $DB->get_record('course', array('id' => $id))) {
+//    error("Course ID is incorrect");
+//}
+//
+//// Wenn man auf Quiz drückt startet kommt man auf die Start Seite
+//$PAGE->set_url(new moodle_url('/mod/activequiz/index.php', array('id' => $course->id)));
+//require_course_login($course);
+//$PAGE->set_pagelayout('incourse');
+//
+//
+//
+///// Get all required strings
+//$stractivequizzes = get_string("modulenameplural", "activequiz");
+//$stractivequiz = get_string("modulename", "activequiz");
+//
+//// Navigation wird hinzugefügt
+//$PAGE->navbar->add($stractivequizzes);
+//// Titel vom Quiz wird hinzugefügt
+//$PAGE->set_title(strip_tags($course->shortname . ': ' . $stractivequizzes));
+//
+//
+////$PAGE->set_heading($course->fullname);
+//echo $OUTPUT->header();
+//
+///// Get all the appropriate data
+//
+//if (!$activequizs = get_all_instances_in_course("activequiz", $course)) {
+//    notice("There are no activequizes", "../../course/view.php?id=$course->id");
+//    die;
+//}
+//
+///// Print the list of instances (your module will probably extend this)
+//
+//$timenow = time();
+//$strname = get_string("name");
+//$strweek = get_string("week");
+//$strtopic = get_string("topic");
+//
+//
+//$table = new html_table();
+//
+//if ($course->format == "weeks") {
+//    $table->head = array($strweek, $strname);
+//    $table->align = array("center", "left");
+//} else if ($course->format == "topics") {
+//    $table->head = array($strtopic, $strname);
+//    $table->align = array("center", "left");
+//} else {
+//    $table->head = array($strname);
+//    $table->align = array("left", "left");
+//}
+//
+//foreach ($activequizs as $activequiz) {
+//    $url = new moodle_url('/mod/activequiz/view.php', array('id' => $activequiz->coursemodule));
+//    if (!$activequiz->visible) {
+//        //Show dimmed if the mod is hidden
+//        $link = '<a class="dimmed" href="' . $url . '">' . $activequiz->name . '</a>';
+//    } else {
+//        //Show normal if the mod is visible
+//        $link = '<a href="' . $url . '">' . $activequiz->name . '</a>';
+//    }
+//
+//    if ($course->format == 'weeks' or $course->format == 'topics') {
+//        $table->data[] = array($activequiz->section, $link);
+//    } else {
+//        $table->data[] = array($link);
+//    }
+//}
+//
+//echo html_writer::table($table);
+//
+///// Finish the page
+//
+//echo $OUTPUT->footer();
 
