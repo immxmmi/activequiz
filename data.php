f7d16244d08adb76e348280c5d82176dcd3cd974
immxmmi <immxmmi@gmx.at>
2022-05-20 14:59:25 +0200

    create test data

--- data.php
+++ data.php
@@ -1,59 +1,51 @@
 <?php
 require_once("../../config.php");
 
-echo  "Hello";
+echo  "Hello\n";
 global $DB;
 $course = $DB->get_record('course', array('id'=>1), '*', MUST_EXIST);;
 var_dump($course);
 // SELECT * FROM "public"."mdl_activequiz_attempts";
 echo "<script>console.log('test')</script>";
 
-echo " <head> <script src='js/chartjs/Chart.min.js'></script></head>";
-
 echo "
-<html>
- <body>
+<!DOCTYPE html>
+<hthml>
+    <head>
+        <meta charset='utf-9'>
+        <title></title>
+        <!--<script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js'></script>-->
+        <script src='../js/chartjs/Chart.min.js'></script>
+    </head>
+    <body>
         <div class='container'>
             <canvas id='doughnutChart'></canvas>
         </div>
-<script>
-            var barChart = document.getElementById('barChart').getContext('2d');
+        <script>
+            var doughnutChart = document.getElementById('doughnutChart');
+            const skillChart = new Chart(doughnutChart, {
+    type: 'doughnut',
+                    data: {
+        labels: ['Strength', 'Skill', 'Health', 'Speed', 'Luck'],
 
-            const massPopChart = new Chart(barChart, {
-                type: 'bar',
-                data: {
-                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                    datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3, 5, 2, 3],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
+                        datasets: [{
+            label: 'Point',
+                            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+            ],
+                            data: [10, 20, 35, 30, 10],              
+                        }]
+                    },
+                    options: {
+        animation: {
+            animateScale: true
                         }
-                    }
-                }
-        });
+    }
+            });
         </script>
-        </body>
-        </html>
-        ";
+    </body>
+</html> ";
