d724bcbbe4f1c91aec27d15da7fd37cb5e0b4806
immxmmi <immxmmi@gmx.at>
2022-05-20 13:29:30 +0200

    database test

--- databaseTest.php
+++ databaseTest.php
@@ -3,5 +3,6 @@ require_once("../../config.php");
 
 echo  "Hello";
 global $DB;
-echo $course = $DB->get_record('course', array('id'=>1), '*', MUST_EXIST);;
+$course = $DB->get_record('course', array('id'=>1), '*', MUST_EXIST);;
+var_dump($course);
 echo "<script>console.log('test')</script>";
