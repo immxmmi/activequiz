a132816af0616750c93266a1917c3b165cbe43bc
immxmmi <immxmmi@gmx.at>
2022-05-20 14:59:51 +0200

    create test data

--- data.php
+++ data.php
@@ -4,7 +4,6 @@ require_once("../../config.php");
 echo  "Hello\n";
 global $DB;
 $course = $DB->get_record('course', array('id'=>1), '*', MUST_EXIST);;
-var_dump($course);
 // SELECT * FROM "public"."mdl_activequiz_attempts";
 echo "<script>console.log('test')</script>";
 
