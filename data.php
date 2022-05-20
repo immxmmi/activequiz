0beff4c1b0792c96b897bdd04a9f1a975f38e8fb
immxmmi <immxmmi@gmx.at>
2022-05-20 15:06:18 +0200

    create test data

--- data.php
+++ data.php
@@ -8,8 +8,6 @@ echo "<script>console.log('test')</script>";
 
 
 
-
-
 echo "
 <!DOCTYPE html>
 <hthml>
@@ -21,33 +19,43 @@ echo "
     </head>
     <body>
         <div class='container'>
-            <canvas id='doughnutChart'></canvas>
+            <canvas id='barChart'></canvas>
         </div>
         <script>
-            var doughnutChart = document.getElementById('doughnutChart');
-            const skillChart = new Chart(doughnutChart, {
-    type: 'doughnut',
-                    data: {
-        labels: ['Strength', 'Skill', 'Health', 'Speed', 'Luck'],
-
-                        datasets: [{
-            label: 'Point',
-                            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-            ],
-                            data: [10, 20, 35, 30, 10],              
-                        }]
-                    },
-                    options: {
-        animation: {
-            animateScale: true
+      const massPopChart = new Chart(barChart, {
+                type: 'bar',
+                data: {
+                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    datasets: [{
+                        label: '# of Votes',
+                        data: [12, 19, 3, 5, 2, 3],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
                         }
-    }
-            });
+                    }
+                }
+        });
         </script>
     </body>
 </html> ";
