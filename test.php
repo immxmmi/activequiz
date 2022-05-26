20a36adf0a32f43263899d3e88e5dc704d197204
immxmmi <immxmmi@gmx.at>
2022-05-26 14:12:07 +0000

    diagram

--- test.php
+++ test.php
@@ -45,9 +45,10 @@ $sessions = $DB->get_records_sql($sql, $params);
 $bestanden = 0;
 $nichtbestanden = 0;
 
+
+// TABLE :: mdl_activequiz_sessions
 echo "<br />";
 echo "TEST DATA TABLE SESSIONS:";
-
 foreach ($sessions as $session)
 {
 
@@ -92,9 +93,9 @@ echo "created: ";
 echo $session->created;
 echo "<br />";
 echo "<br />";
+#######################################################################
 
-
-
+// TABLE :: mdl_activequiz_questions
 echo "<br />";
 echo "TEST DATA TABLE QUESTIONS:";
 
@@ -113,7 +114,8 @@ echo "ActivequizID: ";
 echo $answer->activequizid;
 echo "<br />";
 echo "QuestionID: ";
-echo $answer->questionid;
+$questionID = $answer->questionid;
+echo $questionID;
 echo "<br />";
 echo "NoTime: ";
 echo $answer->notime;
@@ -131,10 +133,176 @@ echo "Showhistoryduringquiz: ";
 echo $answer->showhistoryduringquiz;
 echo "<br />";
 echo "<br />";
+ ###################################################################
+
+echo "TEST QUIZ QUESTIONS:";
+// TABLE :: mdl_question_attempts
+$sql = 'SELECT * FROM "public"."mdl_question_attempts" WHERE questionid = :question;';
+$params = array('question' => $questionID);
+$questions = $DB->get_records_sql($sql, $params);
+
+
+
+############### -> LABELS --> ANSWERS
+$lab1 = "test";
+$lab2 = "test";
+$lab3 = "test";
+$lab4 = "test";
+foreach ($questions as $question)
+{
+    echo "<br />";
+echo "QUESTIONS:";
+echo "<br />";
+echo "ID: ";
+echo $question->id;
+echo "<br />";
+echo "QuestionID: ";
+echo $question->questionid;
+echo "<br />";
+echo "QuestionusageID: ";
+echo $question->questionusageid;
+echo "<br />";
+echo "Question Summary: ";
+$rowallAnswers = $question->questionsummary;
+$allAnswers = explode(':', $rowallAnswers);
+$answerArray = explode(';', $allAnswers[1]);
+
+foreach($answerArray as $option){
+    echo $option;
+}
+
+$allAnswers = $question->questionsummary;
+$lab1 = $answerArray[0];
+$lab2 = $answerArray[1];
+$lab3 = $answerArray[2];
+$lab4 = $answerArray[2];
+
+//echo $allAnswers;
+
+echo "<br />";
+echo "Answer: ";
+echo $question->rightanswer;
+echo "<br />";
+echo "responsesummary: ";
+echo $question->responsesummary;
+echo "<br />";
+echo "<br />";
+echo "<br />";
+
+
+
+$label1 = "test";
+$value1= 2;
+$label2 = "ts";
+$value2= 3;
+$label3 = "tew";
+$value3= 1;
+$label4 = "test";
+$value4= 3;
+
+
+
+echo "
+<!DOCTYPE html>
+<hthml>
+    <head>
+        <meta charset='utf-9'>
+        <title></title>
+        <!--<script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js'></script>-->
+        <script src='js/chartjs/Chart.min.js'></script>
+    </head>
+    <body>
+        <div class='container'>
+            <canvas id='barChart'></canvas>
+        </div>
+        <script>
+      const massPopChart = new Chart(barChart, {
+                type: 'bar',
+                data: {
+                    labels: ['$label1', '$label2', '$label3', '$label4'],
+                    datasets: [{
+                        label: '# of Votes',
+                        data: [$value1, $value2, $value3, $value4],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+        });
+        </script>
+    </body>
+</html> ";
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
 
+echo "<br />";
+echo "<br />";
+
+
+###################################################################
+
+
+
+
+// TABLE :: md_question_answers
+$sql = 'SELECT * FROM "public"."mdl_question_answers" WHERE question = :question;';
+$params = array('question' => $questionID);
+$questions = $DB->get_records_sql($sql, $params);
+
+foreach ($questions as $answer)
+{
+    echo "<br />";
+echo "TEST QUIZ ANSWER:";
+echo "<br />";
+echo "ID: ";
+echo $answer->id;
+
+echo "<br />";
+echo "Question: ";
+echo $answer->question;
+
+echo "<br />";
+echo "Answer: ";
+$clean_answer = strip_tags($answer->answer);
+echo $clean_answer;
+
+echo "<br />";
+echo "<br />";
+}
+}
+
+//#############################################################################
+
+// TABLE :: mdl_activequiz_grades
 echo "<br />";
 echo "TEST DATA TABLE GRADE:";
 
@@ -208,21 +376,6 @@ echo "<br />";
 //$value5= 2;
 //$label6 = "Orange";
 //$value6= 2;
-
-
-$label1 = "Nicht Bestanden";
-$value1= $nichtbestanden;
-//$label2 = "Blue";
-//$value2= 2;
-//$label3 = "Yellow";
-//$value3= 2;
-$label2 = "Bestanden";
-$value2= $bestanden;
-//$label5 = "Purple";
-//$value5= 2;
-//$label6 = "Orange";
-//$value6= 2;
-
 /*
 echo "
 <!DOCTYPE html>
@@ -276,46 +429,3 @@ echo "
     </body>
 </html> ";
 */
-echo "
-<!DOCTYPE html>
-<hthml>
-    <head>
-        <meta charset='utf-9'>
-        <title></title>
-        <!--<script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js'></script>-->
-        <script src='js/chartjs/Chart.min.js'></script>
-    </head>
-    <body>
-        <div class='container'>
-            <canvas id='barChart'></canvas>
-        </div>
-        <script>
-      const massPopChart = new Chart(barChart, {
-                type: 'bar',
-                data: {
-                    labels: ['$label1', '$label2'],
-                    datasets: [{
-                        label: '# of Votes',
-                        data: [$value1, $value2],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(75, 192, 192, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(75, 192, 192, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
-                }
-        });
-        </script>
-    </body>
-</html> ";
