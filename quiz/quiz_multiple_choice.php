883a81380975054953d5e9e3a275334b0b7fdeb6
immxmmi <immxmmi@gmx.at>
2022-05-27 07:03:44 +0200

    quiz

--- quiz/quiz_multiple_choice.php
+++ quiz/quiz_multiple_choice.php
@@ -18,10 +18,12 @@ $sessions = $session->getSessionByID($result);
 
 
 // TABLE :: mdl_activequiz_attempts
+$active_attemp = new activequiz_attempts();
 $sql = 'SELECT * FROM "public"."mdl_activequiz_attempts" WHERE  sessionid = :sessionid;';
 $params = array('sessionid' => $sessions[0]->getId());
 $result = $DB->get_records_sql($sql, $params);
-var_dump($result);
+$active_attemps = $active_attemp->getAttemptsByID($result);
+var_dump($active_attemps);
 
 
 
