845a7d3e85ea2cb157c6ca5f0cafd145b7af40c8
immxmmi <immxmmi@gmx.at>
2022-05-27 07:38:47 +0200

    quiz

--- quiz/mdl_question_attempts.php
+++ quiz/mdl_question_attempts.php
@@ -1,3 +1,55 @@
 <?php
 require_once("../../../config.php");
 
+class quiz_attempts
+{
+    private $id;
+    private $questionusageid;
+    private $slot;
+    private $behaviour;
+    private $questionid;
+    private $variant;
+    private $maxmark;
+    private $minfraction;
+    private $maxfraction;
+    private $flagged;
+    private $questionsummary;
+    private $rightanswer;
+    private $responsesummary;
+    private $timemodified;
+
+
+    public function __construct(){}
+    public function getAttemptsByQuestionengID($result){
+        $attempts = array();
+        $currentAttempt = new quiz_attempts();
+        foreach ($result as $attempt) {
+             $currentAttempt->id=$attempt->id;
+             $currentAttempt->questionusageid=$attempt->questionusageid;
+             $currentAttempt->slot=$attempt->slot;
+             $currentAttempt->behaviour=$attempt->behaviour;
+             $currentAttempt->questionid=$attempt->questionid;
+             $currentAttempt->variant=$attempt->variant;
+             $currentAttempt->maxmark=$attempt->maxmark;
+             $currentAttempt->minfraction=$attempt->minfraction;
+             $currentAttempt->maxfraction=$attempt->maxfraction;
+             $currentAttempt->flagged=$attempt->flagged;
+             $currentAttempt->questionsummary= filterAnswers($attempt->questionsummary);
+             $currentAttempt->rightanswer=$attempt->rightanswer;
+             $currentAttempt->responsesummary=$attempt->responsesummary;
+             $currentAttempt->timemodified=$attempt->timemodified;
+
+            if ($currentAttempt != null) {
+                array_push($attempts, $currentAttempt);
+            }
+        }
+// $currentAttempts = null;
+
+        return $attempts;
+    }
+
+    private function filterAnswers($rowAnswers){
+        $answers = explode(':', $rowAnswers);
+        return explode(';', $answers[1]);
+    }
+}
--- quiz/quiz_multiple_choice.php
+++ quiz/quiz_multiple_choice.php
@@ -25,9 +25,17 @@ $sql = 'SELECT * FROM "public"."mdl_activequiz_attempts" WHERE  sessionid = :ses
 $params = array('sessionid' => $sessions[0]->getId());
 $result = $DB->get_records_sql($sql, $params);
 $active_attemps = $active_attemp->getAttemptsByID($result);
-var_dump($active_attemps[0]);
+//var_dump($active_attemps[0]);
 
 
+
+// TABLE :: mdl_question_attempts
+$active_attemp = new activequiz_attempts();
+$sql = 'SELECT * FROM "public"."mdl_question_attempts" WHERE  sessionid = :sessionid;';
+$params = array('sessionid' => $sessions[0]->getId());
+$result = $DB->get_records_sql($sql, $params);
+$active_attemps = $active_attemp->getAttemptsByID($result);
+//var_dump($active_attemps[0]);
 
 
 
