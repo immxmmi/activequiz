e69e15fc06b5e3f6b7a717d08f0229c4bf8537ca
immxmmi <immxmmi@gmx.at>
2022-05-27 07:18:32 +0200

    quiz

--- quiz/mdl_activequiz_attempts.php
+++ quiz/mdl_activequiz_attempts.php
@@ -4,51 +4,51 @@ require_once("../../../config.php");
 
 class activequiz_attempts
 {
-private $id;
-private $sessionid;
-private $userid;
-private $attemptnum;
-private $questionengid;
-private $status;
-private $preview;
-private $responded;
-private $responded_count;
-private $forgroupid;
-private $timestart;
-private $timefinish;
-private $timemodified;
+    private $id;
+    private $sessionid;
+    private $userid;
+    private $attemptnum;
+    private $questionengid;
+    private $status;
+    private $preview;
+    private $responded;
+    private $responded_count;
+    private $forgroupid;
+    private $timestart;
+    private $timefinish;
+    private $timemodified;
 
 
-public function __construct(){}
+    public function __construct()
+    {
+    }
 
-public function getAttemptsByID($result)
-{
-$attempts = array();
-$currentAttempt= new activequiz_attempts();
-foreach ($result as $attempt) {
-    echo "</br>";
-    //var_dump($attempt);
-    $currentAttempt->id = $attempt->id;
-// $currentAttempts->sessionid = $attempt->sessionid;
-// $currentAttempts->userid = $attempt->userid;
-// $currentAttempts->attemptnum = $attempt->attemptnum;
-// $currentAttempts->questionengid = $attempt->questionengid;
-// $currentAttempts->status = $attempt->status;
-// $currentAttempts->preview = $attempt->preview;
-// $currentAttempts->responded = $attempt->responded;
-// $currentAttempts->responded_count = $attempt->responded_count;
-// $currentAttempts->forgroupid = $attempt->forgroupid;
-// $currentAttempts->timestart = $attempt->timestart;
-// $currentAttempts->timefinish = $attempt->timefinish;
-// $currentAttempts->timemodified = $attempt->timemodified;
-//
- if ($currentAttempt != null) {
-     array_push($attempts, $currentAttempt);
- }
-}
+    public function getAttemptsByID($result)
+    {
+        $attempts = array();
+        $currentAttempt = new activequiz_attempts();
+        foreach ($result as $attempt) {
+            $currentAttempt->id = $attempt->id;
+            $currentAttempt->sessionid = $attempt->sessionid;
+            $currentAttempt->userid = $attempt->userid;
+            $currentAttempt->attemptnum = $attempt->attemptnum;
+            $currentAttempt->questionengid = $attempt->questionengid;
+            $currentAttempt->status = $attempt->status;
+            $currentAttempt->preview = $attempt->preview;
+            $currentAttempt->responded = $attempt->responded;
+            $currentAttempt->responded_count = $attempt->responded_count;
+            $currentAttempt->forgroupid = $attempt->forgroupid;
+            $currentAttempt->timestart = $attempt->timestart;
+            $currentAttempt->timefinish = $attempt->timefinish;
+            $currentAttempt->timemodified = $attempt->timemodified;
+
+            if ($currentAttempt != null) {
+                array_push($attempts, $currentAttempt);
+            }
+        }
 // $currentAttempts = null;
 
-return $attempts;
-}
+        return $attempts;
+    }
 
 }
