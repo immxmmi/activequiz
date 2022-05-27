d19618faccc9218651ecd2e752b94579625fca80
immxmmi <immxmmi@gmx.at>
2022-05-27 11:49:55 +0200

    quiz

--- quiz/quiz_multiple_choice.php
+++ quiz/quiz_multiple_choice.php
@@ -64,7 +64,7 @@ foreach($question_attemps as $summary){
     echo "</br>";
     $labels = $summary->getQuestionsummary();
     $responsesummary = $summary->getResponsesummary();
-    $chart->setCurrentID(strval($index));
+    $chart->setCurrentID("test".strval($index));
     echo "</br>";
     echo "DATA::::";
     $chart->countValue($labels,$values,$responsesummary);
