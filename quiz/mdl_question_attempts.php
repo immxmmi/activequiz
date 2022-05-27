8c28a344eece92aad3c69dfcd0e39ff0ed9ebe95
immxmmi <immxmmi@gmx.at>
2022-05-27 07:52:07 +0200

    quiz

--- quiz/mdl_question_attempts.php
+++ quiz/mdl_question_attempts.php
@@ -19,36 +19,40 @@ class question_attempts
     private $timemodified;
 
 
-    public function __construct(){}
-    public function getAttemptsByQuestionengID($result){
+    public function __construct()
+    {
+    }
+
+    public function getAttemptsByQuestionengID($result)
+    {
         $attempts = array();
         $currentAttempt = new question_attempts();
         foreach ($result as $attempt) {
-            $currentAttempt->id=$attempt->id;
-            $currentAttempt->questionusageid=$attempt->questionusageid;
-           // $currentAttempt->slot=$attempt->slot;
-           // $currentAttempt->behaviour=$attempt->behaviour;
-           // $currentAttempt->questionid=$attempt->questionid;
-           // $currentAttempt->variant=$attempt->variant;
-           // $currentAttempt->maxmark=$attempt->maxmark;
-           // $currentAttempt->minfraction=$attempt->minfraction;
-           // $currentAttempt->maxfraction=$attempt->maxfraction;
-           // $currentAttempt->flagged=$attempt->flagged;
-           // $currentAttempt->questionsummary= filterAnswers($attempt->questionsummary);
-           // $currentAttempt->rightanswer=$attempt->rightanswer;
-           // $currentAttempt->responsesummary=$attempt->responsesummary;
-           // $currentAttempt->timemodified=$attempt->timemodified;
-
-           if ($currentAttempt != null) {
-               array_push($attempts, $currentAttempt);
-           }
+            $currentAttempt->id = $attempt->id;
+            $currentAttempt->questionusageid = $attempt->questionusageid;
+            $currentAttempt->slot = $attempt->slot;
+            $currentAttempt->behaviour = $attempt->behaviour;
+            $currentAttempt->questionid = $attempt->questionid;
+            $currentAttempt->variant = $attempt->variant;
+            $currentAttempt->maxmark = $attempt->maxmark;
+            $currentAttempt->minfraction = $attempt->minfraction;
+            $currentAttempt->maxfraction = $attempt->maxfraction;
+            $currentAttempt->flagged = $attempt->flagged;
+            $currentAttempt->questionsummary = filterAnswers($attempt->questionsummary);
+            $currentAttempt->rightanswer = $attempt->rightanswer;
+            $currentAttempt->responsesummary = $attempt->responsesummary;
+            $currentAttempt->timemodified = $attempt->timemodified;
+            if ($currentAttempt != null) {
+                array_push($attempts, $currentAttempt);
+            }
         }
 // $currentAttempts = null;
 
         return $attempts;
     }
 
-    private function filterAnswers($rowAnswers){
+    private function filterAnswers($rowAnswers)
+    {
         $answers = explode(':', $rowAnswers);
         return explode(';', $answers[1]);
     }
