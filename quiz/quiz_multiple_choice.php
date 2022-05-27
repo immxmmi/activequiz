3c9568550126ba02972dcd275398bbf02b096880
immxmmi <immxmmi@gmx.at>
2022-05-27 09:05:36 +0200

    quiz

--- quiz/quiz_multiple_choice.php
+++ quiz/quiz_multiple_choice.php
@@ -53,6 +53,53 @@ $chart = new Chart();
 $chart->output("aa","vv","cc","dd",2,3,4,5);
 
 
+echo "
+    <!DOCTYPE html>
+    <hthml>
+        <head>
+            <meta charset='utf-9'>
+            <title></title>
+            <!--<script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js'></script>-->
+            <script src='js/chartjs/Chart.min.js'></script>
+        </head>
+        <body>
+            <div class='container'>
+                <canvas id='barChart'></canvas>
+            </div>
+            <script>
+        const massPopChart = new Chart(barChart, {
+                    type: 'bar',
+                    data: {
+                        labels: ['aa', 'aa', 'aa', 'cc'],
+                        datasets: [{
+                            label: '# of Votes',
+                            data: [2, 3, 4, 2],
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+            });
+            </script>
+        </body>
+    </html> ";
 
 
 
