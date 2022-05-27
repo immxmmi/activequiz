ddd229568b84449a8b9c1b296c54a50b96386d38
immxmmi <immxmmi@gmx.at>
2022-05-27 05:40:02 +0200

    quiz

--- quiz/quiz_multiple_choice.php
+++ quiz/quiz_multiple_choice.php
@@ -2,3 +2,14 @@
 require_once("../../../config.php");
 require_once("../classes/activequiz_session.php");
 global $DB;
+
+
+// TABLE :: mdl_activequiz_sessions
+// SESSION
+$sessionID = 0;
+
+$sql = 'SELECT * FROM "public"."mdl_activequiz_sessions" WHERE id = :sessionid';
+$params = array('sessionid' => $sessionID);
+$sessions = $DB->get_records_sql($sql, $params);
+
+var_dump($sessionID);
