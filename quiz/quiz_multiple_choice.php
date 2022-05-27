8e9ad64a9ef9755411fcbb32b0786fc9d87c8cf3
immxmmi <immxmmi@gmx.at>
2022-05-27 07:47:31 +0200

    quiz

--- quiz/quiz_multiple_choice.php
+++ quiz/quiz_multiple_choice.php
@@ -34,6 +34,7 @@ $question_attemp = new question_attempts();
 $sql = 'SELECT * FROM "public"."mdl_question_attempts" WHERE  questionusageid = :questionusageid;';
 $params = array('questionusageid' => $active_attemps[0]->getQuestionengid());
 $result = $DB->get_records_sql($sql, $params);
+var_dump($result);
 //$question_attemps = $question_attemp->getAttemptsByQuestionengID($result);
 //var_dump($question_attemps[0]);
 
