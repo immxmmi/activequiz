759761310895d7a6057f578516154d380450a200
immxmmi <immxmmi@gmx.at>
2022-06-01 14:08:31 +0200

    quiz

--- quiz/chart_builder.php
+++ quiz/chart_builder.php
@@ -85,23 +85,6 @@ class Chart
            
         </head>
         <body>';
-
-        echo '<div>
-			<form action="javascript:void(0);">
-				<select id="charttype" name="type">
-					<option value="none">--- choose a chart ---</option>
-					<option value="pie">Pie-Chart</option>
-					<option value="bar">Bar-Chart</option>
-					<option value="doughnut">Doughnut-Chart</option>
-					<option value="unknown">Unknown-Chart</option>
-				</select>
-			</form>
-		</div>
-        <div class="container">
-			<div class="chartwrapper">
-				<canvas id="apiChart"></canvas>
-			</div>
-        </div>';
     }
 
 
@@ -200,6 +183,38 @@ class Chart
 
     public function buildNewBar($labels, $values)
     {
+
+        echo '<div>
+			<form action="javascript:void(0);">
+				<select id="charttype" name="type">
+					<option value="none">--- choose a chart ---</option>
+					<option value="pie">Pie-Chart</option>
+					<option value="bar">Bar-Chart</option>
+					<option value="doughnut">Doughnut-Chart</option>
+					<option value="unknown">Unknown-Chart</option>
+				</select>
+			</form>
+		</div>
+        <div class="container">
+			<div class="chartwrapper">
+				<canvas id="apiChart"></canvas>
+			</div>
+        </div>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         return $data = array(
             'labels' => $labels,
             'datasets' => array(
