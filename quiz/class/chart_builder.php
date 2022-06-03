eb841874096767a179eb77d2541d0c8bffabcd56
immxmmi <immxmmi@gmx.at>
2022-06-03 18:25:36 +0200

    quiz

--- quiz/class/chart_builder.php
+++ quiz/class/chart_builder.php
@@ -61,7 +61,7 @@ class chart_builder
         $this->chartType = $chartType;
         //$color =  dec2hex(rand(0,255)).dec2hex(rand(0,255)).dec2hex( rand(0,255));
 
-        $color =  'rgba('.rand(0,255).','.rand(0,255).','.rand(0,255).','.rand(0.0,1);
+        $color =  'rgba('.rand(0,255).','.rand(0,255).','.rand(0,255).','.rand(0.0,1).')';
 
 
         switch ($chartType) {
