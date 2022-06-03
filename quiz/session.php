6ed5c6a433e9450e210c969a6b2b6c1aaae78924
immxmmi <immxmmi@gmx.at>
2022-06-03 17:24:50 +0200

    quiz

--- quiz/session.php
+++ quiz/session.php
@@ -34,7 +34,7 @@ class Session
     public function __construct()
     {
         $sql = 'SELECT * FROM "public"."mdl_activequiz_sessions" WHERE id = :sessionid';
-        $params = array('sessionid' => $sessionid);
+        $params = array('sessionid' => 46);
         $result = $DB->get_records_sql($sql, $params);
     }
 
