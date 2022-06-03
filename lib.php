53830389de3223799203bd3f47d25c2eff8cea93
immxmmi <immxmmi@gmx.at>
2022-06-04 00:06:19 +0200

    quiz

--- lib.php
+++ lib.php
@@ -170,9 +170,9 @@ function activequiz_grade_item_update($activequiz, $grades = null) {
     $activequiz = json_decode(json_encode($activequiz), true);
 
     if (array_key_exists('cmidnumber', $activequiz)) { // May not be always present.
-        $params = array('itemname' => $activequiz['name'], 'idnumber' => $activequiz->cmidnumber);
+        $params = array('itemname' => $activequiz->name, 'idnumber' => $activequiz->cmidnumber);
     } else {
-        $params = array('itemname' => $activequiz['name']);
+        $params = array('itemname' => $activequiz->name);
     }
 
     if ($activequiz->graded == 0) {
