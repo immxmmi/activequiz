cb843a5580627d1d01cc5402d3644f9fe9128b9d
immxmmi <immxmmi@gmx.at>
2022-06-03 16:15:31 +0200

    quiz

--- quiz/activequizapi.php
+++ quiz/activequizapi.php
@@ -17,93 +17,41 @@ $activequizID = 18;
 
 // CHART BUILDER
 $chart = new Chart();
-// CHART HEAD
-$chart->startTag();
-
 
 # # # # # # # # -SESSION- # # # # # # # #
-echo "SESSION DATA:</br>";
-echo "TABLE :: mdl_activequiz_sessions</br>";
 $session = new Session();
 $sql = 'SELECT * FROM "public"."mdl_activequiz_sessions" WHERE id = :sessionid AND activequizid = :activequizid';
 $params = array('sessionid' => $sessionID, 'activequizid' => $activequizID);
 $result = $DB->get_records_sql($sql, $params);
 $current_session = $session->getSessionByID($result);
-
-echo"</br>";
-echo"</br>";
-echo "SESSION ID: ".$current_session->getId();
-echo"</br>";
-echo"Current Question: ".$current_session->getCurrentquestion();
-echo"</br>";
-echo"</br>";
 ##########################################
 
 
 # # # # # # # #  -ACTIVE-QUIZ ATTEMPTS- # # # # # # # #
-echo "ACTIVE-QUIZ ATTEMPTS</br>";
-echo "TABLE :: mdl_activequiz_attempts</br>";
 $active_attemp = new activequiz_attempts();
 $sql = 'SELECT * FROM "public"."mdl_activequiz_attempts" WHERE  sessionid = :sessionid;';
 $params = array('sessionid' => $current_session->getId());
 $result = $DB->get_records_sql($sql, $params);
 $active_attemps = $active_attemp->getAttemptsByID($result);
 $current_attemp = $active_attemps[0];
-
-
-
-
-echo"</br>";
-echo"</br>";
-echo"</br>";
-echo"QUBALAYOUT: ".$current_attemp->getQubalayout();
-echo"</br>";
-echo"ATTEMPTNUM: ".$current_attemp->getAttemptnum();
-echo"</br>";
-echo"questionengid: ".$current_attemp->getQuestionengid();
-echo"</br>";
-echo"PREVIEW: ".$current_attemp->getPreview();
-echo"</br>";
-echo"</br>";
-echo "QID:";
-echo"</br>";
 $all_questionengids = $active_attemp->filterQID($active_attemps);
-var_dump($all_questionengids);
-echo"</br>";
-//var_dump($active_attemps);
 #######################################################
-
 
 
 # # # # # # # #  -QUESTION ATTEMPTS- # # # # # # # #
 $list_of_question_attemps = array(); // LIST ATTEMPS
 $current_slot = $current_session->getCurrentquestion(); // SLOT
 $current_slot = 2; // SLOT
-
-
-echo "QUESTION ATTEMPTS</br>";
-echo "TABLE :: mdl_question_attempts</br>";
-
 foreach ($all_questionengids as $questionengids) {
-        $question_attemp = new question_attempts();
-        $sql = 'SELECT * FROM "public"."mdl_question_attempts" WHERE  questionusageid = :questionusageid AND slot= :slot';
-        $params = array('questionusageid' => $questionengids, 'slot' => $current_slot);
-        $result = $DB->get_records_sql($sql, $params);
-        $question_attemps = $question_attemp->getAttemptsByQuestionengID($result);
+    $question_attemp = new question_attempts();
+    $sql = 'SELECT * FROM "public"."mdl_question_attempts" WHERE  questionusageid = :questionusageid AND slot= :slot';
+    $params = array('questionusageid' => $questionengids, 'slot' => $current_slot);
+    $result = $DB->get_records_sql($sql, $params);
+    $question_attemps = $question_attemp->getAttemptsByQuestionengID($result);
     //var_dump($question_attemps);
     ####################################################
     array_push($list_of_question_attemps, $question_attemps);
 }
-echo"</br>";
-echo"</br>";
-echo"</br>";
-echo"</br>";
-echo "<pre>";
-print_r($list_of_question_attemps);
-echo"</pre>";
-echo"</br>";
-echo"</br>";
-echo"</br>";
 
 $chartType = "";
 $questionType = "singel";
@@ -115,15 +63,6 @@ $trueFalse = new TrueFalse_Choice();
 switch ($questionType) {
     case "singel":
         $single->setData($list_of_question_attemps[0]);
-        echo "</br> VALUES";
-        echo "<pre>";
-        print_r($single->getValues());
-        echo "</pre>";
-        echo "</br> LABELS";
-        echo "</br>";
-        echo "<pre>";
-        print_r($single->getLabels());
-        echo "</pre>";
         break;
     case "true/false":
         $trueFalse->setData($list_of_question_attemps[0]);
@@ -147,32 +86,9 @@ switch (1) {
 }
 
 $data = $chart->buildNewChart($chartType, $single->getLabels(), $single->getValues());
-
-
-echo "</br>";
 echo json_encode($data, JSON_PRETTY_PRINT);
+exit;
 
-
-
-
-
-echo "<head><script>
-var ourChart = null;
-var showChart = null;
-var ourData = ".json_encode($data).";
-		jQuery(document).ready(function () {
-				ourChart = jQuery('#ourChart');
-                showChart = new Chart(ourChart,{
-                    type: ourData.data.charttype,
-					data: ourData.data.chartdata,
-					options: ourData.data.chartoptions
-                });
-			});
-</script></head>";
-
-echo '<body><div class="container">
-            <canvas id="ourChart"></canvas> 
-        </div></body>';
 
 
 
