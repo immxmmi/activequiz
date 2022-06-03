2d257d643440cae695420873581b03c9b022a4d0
immxmmi <immxmmi@gmx.at>
2022-06-03 18:16:11 +0200

    quiz

--- quiz/class/chart_builder.php
+++ quiz/class/chart_builder.php
@@ -71,7 +71,7 @@ class chart_builder
                             'label' => '# of Votes',
                             'data' => $values,
                             'backgroundColor' => array(
-                                'rgba(255, 99, 132, 0.2)',
+                                $color,
                                 'rgba(54, 162, 235, 0.2)',
                                 'rgba(255, 206, 86, 0.2)',
                                 'rgba(75, 192, 192, 0.2)',
