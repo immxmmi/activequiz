8ed424e6df57b8be4728ffc7aa91b657f586a6bc
immxmmi <immxmmi@gmx.at>
2022-06-04 02:39:17 +0200

    quiz

--- quiz/class/mdl_question_attempt_step_data.php
+++ quiz/class/mdl_question_attempt_step_data.php
@@ -2,6 +2,7 @@
 require_once("../../../config.php");
 require_once("mdl_question_attempt_steps.php");
 global $DB;
+
 class attempt_step_data
 {
 
@@ -9,24 +10,30 @@ class attempt_step_data
     private $attemptstepid;
     private $name;
 
+    private $step_data_list = array();
+
     public function __construct($steps_attempts)
     {
         global $DB;
+        if ($steps_attempts !== null) {
 
-        foreach ($steps_attempts as $steps) {
-            foreach ($steps as $step) {
-            $sql = 'SELECT * FROM "public"."mdl_question_attempt_step_data" WHERE attemptstepid = :attemptstepid';
-            $params = array('attemptstepid' => $step->id);
-            $step_data= $DB->get_records_sql($sql, $params);
-                foreach($step_data as $data){
-                    echo "<pre>";
-                    print_r($data);
-                    echo "</pre>";
+            foreach ($steps_attempts as $steps) {
+                foreach ($steps as $step) {
+                    $sql = 'SELECT * FROM "public"."mdl_question_attempt_step_data" WHERE attemptstepid = :attemptstepid';
+                    $params = array('attemptstepid' => $step->id);
+                    $step_data = $DB->get_records_sql($sql, $params);
+                    foreach ($step_data as $data) {
+                        array_push($this->step_data_list, $data);
+                    }
                 }
-                //      array_push($this->attemptstepids,$result);
-                //   }
             }
         }
+
+        echo "<pre>";
+        echo print_r($this->step_data_list);
+        echo "</pre>";
     }
+
+
 
 }
