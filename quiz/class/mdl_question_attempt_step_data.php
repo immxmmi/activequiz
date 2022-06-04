b75ed47e7721a2600e2b5e7cb5591cd0661dc1fa
immxmmi <immxmmi@gmx.at>
2022-06-04 03:38:48 +0200

    quiz

--- quiz/class/mdl_question_attempt_step_data.php
+++ quiz/class/mdl_question_attempt_step_data.php
@@ -21,7 +21,7 @@ class attempt_step_data
 
             foreach ($steps_attempts as $step) {
                      echo "<pre>";
-                       print_r($step);
+                       print_r($step->id);
                        echo "</pre>";
 
 
