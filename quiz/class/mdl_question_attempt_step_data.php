19fe18a238b23be24318fe27fd6e33b274195e0a
immxmmi <immxmmi@gmx.at>
2022-06-04 02:30:49 +0200

    quiz

--- quiz/class/mdl_question_attempt_step_data.php
+++ quiz/class/mdl_question_attempt_step_data.php
@@ -9,19 +9,19 @@ class attempt_step_data
     private $attemptstepid;
     private $name;
 
-    public function __construct($steps)
+    public function __construct($steps_attempts)
     {
         global $DB;
 
-        foreach ($steps as $step) {
-            foreach ($step as $class) {
+        foreach ($steps_attempts as $steps) {
+            foreach ($steps as $step) {
             $sql = 'SELECT * FROM "public"."mdl_question_attempt_step_data" WHERE attemptstepid = :attemptstepid';
-            $params = array('attemptstepid' => $class->id);
+            $params = array('attemptstepid' => $step->id);
             $result = $DB->get_records_sql($sql, $params);
                 //      array_push($this->attemptstepids,$result);
                 //   }
                 echo "<pre>";
-                print_r($class);
+                print_r($step);
                 echo "</pre>";
             }
         }
