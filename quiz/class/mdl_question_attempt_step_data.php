6134034360812e114423f02a8c031bb1c77cbd1f
immxmmi <immxmmi@gmx.at>
2022-06-04 03:56:39 +0200

    quiz

--- quiz/class/mdl_question_attempt_step_data.php
+++ quiz/class/mdl_question_attempt_step_data.php
@@ -25,7 +25,7 @@ class attempt_step_data
                 $params = array('attemptstepid' => $step->getId());
                 $step_data = $DB->get_records_sql($sql, $params);
                 echo "<pre>";
-                print_r($step);
+                print_r($step->answer_list);
                 echo "</pre>";
                 foreach ($step_data as $data) {
                    // $cur = $this->build($data->id,$data->attemptstepid,$data->name,$data->value,$step->answer_list);
