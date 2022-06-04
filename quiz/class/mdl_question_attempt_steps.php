2c5f3799f257cbb1a1f2e5800fea30a422af673b
immxmmi <immxmmi@gmx.at>
2022-06-04 02:01:49 +0200

    quiz

--- quiz/class/mdl_question_attempt_steps.php
+++ quiz/class/mdl_question_attempt_steps.php
@@ -19,8 +19,8 @@ class attempt_steps
         global $DB;
 
         foreach ($answers as $questionattemptid) {
-            $sql = 'SELECT * FROM "public"."mdl_question_attempt_steps" WHERE questionattemptid = :questionattemptid';
-            $params = array('questionattemptid' => $questionattemptid[0]->getid());
+            $sql = 'SELECT * FROM "public"."mdl_question_attempt_steps" WHERE questionattemptid = :questionattemptid AND sequencenumber != :sequencenumber';
+            $params = array('questionattemptid' => $questionattemptid[0]->getid(), 'sequencenumber' => 0);
             $result = $DB->get_records_sql($sql, $params);
 
             echo"<pre>";
