d293f35c69b129d011150f1f6a0d4970abb06716
immxmmi <immxmmi@gmx.at>
2022-06-04 03:08:46 +0200

    quiz

--- quiz/class/mdl_question_attempt_steps.php
+++ quiz/class/mdl_question_attempt_steps.php
@@ -29,7 +29,7 @@ class attempt_steps
             echo"</pre>";
 
             echo"<pre>";
-            print_r($questionattemptid->getQuestionsummary());
+            //print_r($questionattemptid->getQuestionsummary());
             echo"</pre>";
 
            //$currentstep = builder(
