561fba34831224035ee7728b5ab2c8297b0ed814
immxmmi <immxmmi@gmx.at>
2022-06-05 18:47:46 +0200

    add view

--- classes/activequiz_attempt.php
+++ classes/activequiz_attempt.php
@@ -191,7 +191,7 @@ class activequiz_attempt {
      * @return string the HTML fragment for the question
      */
     public function render_question($slotid, $review = false, $reviewoptions = '',$sessionId) {
-        $displayoptions; //= $this->get_display_options($review, $reviewoptions);
+        $displayoptions = $this->get_display_options($review, $reviewoptions);
 
         $questionnum = $this->get_question_number();
         $this->add_question_number();
