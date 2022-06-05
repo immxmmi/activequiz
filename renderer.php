6789dd9500d4e5305324bc4a86b600be02a75ca1
immxmmi <immxmmi@gmx.at>
2022-06-05 19:04:20 +0200

    add view

--- renderer.php
+++ renderer.php
@@ -328,7 +328,7 @@ class mod_activequiz_renderer extends plugin_renderer_base {
         foreach ($attempt->getSlots() as $slot) {
             // render question form.
             //$output .= $this->render_question_form($slot, $attempt);
-            $output .= html_writer::div('', 'testme', '');
+            $output .= html_writer::div('', 'testme', array('id' => 'tophat'));
             $output .= html_writer::end_div();
         }
 
