29718465828894f2f9ad02ea8e9d0adf0befeb90
immxmmi <immxmmi@gmx.at>
2022-06-05 20:18:59 +0200

    add view

---
+++ chart/chart_api.js
@@ -0,0 +1,41 @@
+jQuery(document).ready(function () {
+    apiChart = jQuery('#apiChart');
+    jQuery('#charttype').bind('change', changeChartTypeHandler);
+});
+
+var changeChartTypeHandler = function() {
+    var charttype = jQuery('#charttype').val();
+    var sessionid = jQuery('#sessionid').val();
+
+    if( charttype !== 'none' && sessionid !== '0') {
+        var url = './chart/chart_api.php';
+        var params = {
+            sessionid: sessionid,
+            type: charttype
+        };
+        jQuery.get(url, params, redrawChart).fail(function(data) {
+            destroyChart();
+            alert(data.responseJSON.meta.msg);
+        });
+    }
+};
+
+var destroyChart = function() {
+    if( skillChart !== null ) {
+        skillChart.destroy();
+    }
+};
+
+var redrawChart = function(data) {
+    if( data.meta.status === 'error' ) {
+        alert(data.meta.msg);
+        return;
+    }
+
+    destroyChart();
+    skillChart = new Chart(apiChart, {
+        type: data.data.charttype,
+        data: data.data.chartdata,
+        options: data.data.chartoptions
+    });
+};
