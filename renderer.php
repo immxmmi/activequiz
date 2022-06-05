ba81af4b63fc36b3dd608c1625f74a4f0acfc6ed
immxmmi <immxmmi@gmx.at>
2022-06-05 18:23:58 +0200

    add

--- renderer.php
+++ renderer.php
@@ -334,7 +334,7 @@ class mod_activequiz_renderer extends plugin_renderer_base {
        // echo $output;
 
         echo "<pre>";
-        print_r($session);
+        print_r($session->get_current_userid());
         echo "</pre>";
     }
 
