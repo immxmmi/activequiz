fb46fda5603f10d6b68733d0e8cb89b7f5eaafde
immxmmi <immxmmi@gmx.at>
2022-06-05 21:26:21 +0200

    add view

--- renderer.php
+++ renderer.php
@@ -404,8 +404,7 @@ class mod_activequiz_renderer extends plugin_renderer_base
 
         $output .= '	<div>
 			<form action="javascript:void(0);">
-                <label for="session">Session ID:</label>
-                <input type="number" id="sessionid" name="session" value="'.$sessionID.'">
+                <label for="session" id="sessionid" name="session" value="'.$sessionID.'">Session ID:</label>
 
 
 
