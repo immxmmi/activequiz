85a332e6ab4e5d8f1c0047d8544c7ae00af266fd
immxmmi <immxmmi@gmx.at>
2022-06-05 22:15:55 +0200

    add view

--- chart/index.php
+++ chart/index.php
@@ -6,60 +6,48 @@ require_once("../../../config.php");
     <head>
         <meta charset="utf-8">
         <title>Chart</title>
-		<style type="text/css">
-			.chartwrapper {
-				width: 640px;
-			}
-		</style>
-		<script src="<?php echo $CFG->wwwroot; ?>/lib/jquery/jquery-3.5.1.min.js"></script>
+        <style type="text/css">
+            .chartwrapper {
+                width: 640px;
+            }
+        </style>
+        <script src="<?php echo $CFG->wwwroot; ?>/lib/jquery/jquery-3.5.1.min.js"></script>
         <script src="<?php echo $CFG->wwwroot; ?>/mod/activequiz/js/chartjs/Chart.min.js"></script>
-
-
-
-
-
-        <script src="../js/chart__api.js"></script>
-
+        <script src="../js/chart-api.js"></script>
     </head>
 
 
-
-
     <body>
-		<div>
-			<form action="javascript:void(0);">
-                <label for="session">Session ID:</label>
-                <input type="number" id="sessionid" name="session" value="5">
+    <div>
+        <form action="javascript:void(0);">
+            <label for="session">Session ID:</label>
+            <input type="number" id="sessionid" name="session" value="5">
 
 
+            <label for="slot">Question Slot:</label>
+            <input type="number" id="slot" name="slot" value="1">
 
 
-                <label for="slot">Question Slot:</label>
-                <input type="number" id="slot" name="slot" value="1">
+            <label for="type">Chart Type:</label>
 
 
-                <label for="type">Chart Type:</label>
+            <select id="charttype" name="type">
+                <option value="none">--- choose a chart ---</option>
+                <option value="pie">Pie-Chart</option>
+                <option value="bar">Bar-Chart</option>
+                <option value="doughnut">Doughnut-Chart</option>
+                <option value="unknown">Unknown-Chart</option>
+            </select>
 
 
-				<select id="charttype" name="type">
-					<option value="none">--- choose a chart ---</option>
-					<option value="pie">Pie-Chart</option>
-					<option value="bar">Bar-Chart</option>
-					<option value="doughnut">Doughnut-Chart</option>
-					<option value="unknown">Unknown-Chart</option>
-				</select>
+        </form>
+    </div>
 
-
-
-
-            </form>
+    <div class="container">
+        <div class="chartwrapper">
+            <canvas id="apiChart"></canvas>
         </div>
-
-        <div class="container">
-			<div class="chartwrapper">
-				<canvas id="apiChart"></canvas>
-			</div>
-        </div>
+    </div>
 
     </body>
 </hthml>
