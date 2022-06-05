eb8d561c64949c0242e5f3da02622245a165522b
immxmmi <immxmmi@gmx.at>
2022-06-05 19:14:13 +0200

    add view

--- renderer.php
+++ renderer.php
@@ -331,7 +331,7 @@ class mod_activequiz_renderer extends plugin_renderer_base {
            // $output .= html_writer::div('', '', array('id' => 'chartDiv'));
            // $output .= html_writer::form('', '', array('action' => 'javascript:void(0);'));
             $output .= html_writer::label('Session ID:', '', array('for' => 'session'));
-            $output .= html_writer::end_label();
+            html_writer::end_tag('label');
           //  $output .= html_writer::end_form();
             $output .= html_writer::end_div();
         }
