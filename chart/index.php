523563f8a08fbb7be6c201b281a76c8992718dd2
immxmmi <immxmmi@gmx.at>
2022-06-05 21:40:04 +0200

    add view

--- chart/index.php
+++ chart/index.php
@@ -18,53 +18,7 @@ require_once("../../../config.php");
 
 
 
-		<script>
-			var apiChart = null;
-			var skillChart = null;
-			
-			jQuery(document).ready(function () {
-				apiChart = jQuery('#apiChart');
-				jQuery('#charttype').bind('change', changeChartTypeHandler);
-			});
-			
-			var changeChartTypeHandler = function() {
-				var charttype = jQuery('#charttype').val();
-                var sessionid = jQuery('#sessionid').val();
-                var slot = jQuery('#slot').val();
-				if( charttype !== 'none' && sessionid !== '0') {
-					var url = './chart_api.php';
-					var params = {
-                        sessionid: sessionid,
-                        slot: slot,
-						type: charttype
-					};
-					jQuery.get(url, params, redrawChart).fail(function(data) {
-						destroyChart();
-						alert(data.responseJSON.meta.msg);
-					});
-				}
-			};
-
-			var destroyChart = function() {
-				if( skillChart !== null ) {
-					skillChart.destroy();
-				}	
-			};
-			
-			var redrawChart = function(data) {
-				if( data.meta.status === 'error' ) {
-					alert(data.meta.msg);
-					return;
-				}
-				
-				destroyChart();
-				skillChart = new Chart(apiChart, {
-					type: data.data.charttype,
-					data: data.data.chartdata,
-					options: data.data.chartoptions
-				});
-			};
-        </script>
+        <script src="../js/chart_api.js"></script>
 
     </head>
 
