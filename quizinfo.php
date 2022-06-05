e44442806e7cf6c5948c0cbca6e1745dc75b3728
immxmmi <immxmmi@gmx.at>
2022-06-05 08:36:39 +0200

    quiz

--- quizinfo.php
+++ quizinfo.php
@@ -31,7 +31,7 @@ require_sesskey();
 
 // if they've passed the sesskey information grab the session info
 $sessionid = required_param('sessionid', PARAM_INT);
-
+echo "<script>console.log('test')</script>";
 // get JSONlib to return json response
 $jsonlib = new \mod_activequiz\utils\jsonlib();
 
