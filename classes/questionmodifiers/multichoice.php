b8ce67b780906cce9e25c1cf68e2900aa49f91d7
Momo <immxmmi@gmx.at>
2022-06-09 13:38:59 +0200

    fix

--- classes/questionmodifiers/multichoice.php
+++ classes/questionmodifiers/multichoice.php
@@ -44,7 +44,7 @@ class multichoice implements \mod_activequiz\questionmodifiers\ibasequestionmodi
     public function add_js() {
         global $PAGE;
 
-       // $PAGE->requires->js('/mod/activequiz/js/chart/Chart.min.js');
+        $PAGE->requires->js('/mod/activequiz/js/chart4/Chart.min.js');
 
     }
 
