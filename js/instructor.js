e5634d1617d78565108d77f5a2fa67a2bfd6e274
Momo <immxmmi@gmx.at>
2022-06-17 00:57:01 +0200

    fix

--- js/instructor.js
+++ js/instructor.js
@@ -738,7 +738,6 @@ function create_chart(chart_typ){
     var url = './chart/chart_api.php';
     var params = {
         sessionid: activequiz.get('sessionid'),
-        slot: '0',
         type: chart_typ
     };
     jQuery.get(url, params, redrawChart).fail(function(data) {
@@ -746,28 +745,6 @@ function create_chart(chart_typ){
         alert(data.responseJSON.meta.msg);
     });
 
-    jQuery(document).ready(function () {
-        apiChart = jQuery('#apiChart');
-        jQuery('#charttype').bind('change', changeChartTypeHandler);
-    });
-
-    var changeChartTypeHandler = function() {
-        var charttype = jQuery('#charttype').val();
-        var sessionid = jQuery('#sessionid').val();
-        var slot = jQuery('#slot').val();
-        if( charttype !== 'none' && sessionid !== '0') {
-            var url = './../../chart/chart_api.php';
-            var params = {
-                sessionid: sessionid,
-                slot: slot,
-                type: charttype
-            };
-            jQuery.get(url, params, redrawChart).fail(function(data) {
-                destroyChart();
-                alert(data.responseJSON.meta.msg);
-            });
-        }
-    };
 
     var destroyChart = function() {
         if( skillChart !== null ) {
