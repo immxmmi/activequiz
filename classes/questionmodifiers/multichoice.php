879d6999316c3de017884eb0b1430c80003e7b8e
Momo <immxmmi@gmx.at>
2022-06-17 00:30:36 +0200

    fix

--- classes/questionmodifiers/multichoice.php
+++ classes/questionmodifiers/multichoice.php
@@ -212,8 +212,7 @@ class multichoice implements \mod_activequiz\questionmodifiers\ibasequestionmodi
 
         $chartoutput = html_writer::end_tag('div');
         $chartoutput .= '
-        <div class="activequizbox">
-        <div class="container">
+        <div class="container" id="chartbox">
 			<div class="chartwrapper">
 				<canvas id="apiChart"></canvas>
 			</div>
