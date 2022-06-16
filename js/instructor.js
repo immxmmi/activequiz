5080abf14ca9d3074761afef5d2370d1e0c4f53d
Momo <immxmmi@gmx.at>
2022-06-16 11:16:04 +0200

    fix

--- js/instructor.js
+++ js/instructor.js
@@ -717,3 +717,24 @@ activequiz.clear_and_hide_notresponded = function () {
     }
 
 };
+
+
+activequiz.show_chart = function () {
+
+    alert("test");
+
+    var toggleresponsesBtn = document.getElementById('toggleresponses');
+
+    if (activequiz.get('showstudentresponses') === false) { // if it is false, set it back to true for the student responses to show
+
+        toggleresponsesBtn.innerHTML = M.util.get_string('hidestudentresponses', 'activequiz');
+
+        activequiz.set('showstudentresponses', true);
+        activequiz.gather_current_results();
+    } else { // if it's set to true, or not set at all, then set it to false when this button is clicked
+
+        toggleresponsesBtn.innerHTML = M.util.get_string('showstudentresponses', 'activequiz');
+        activequiz.set('showstudentresponses', false);
+        activequiz.clear_and_hide_qinfobox();
+    }
+};
