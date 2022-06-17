62ec3191be43a67110c53f8a8ad63e0530d43152
Momo <immxmmi@gmx.at>
2022-06-17 10:37:15 +0200

    fix

--- renderer.php
+++ renderer.php
@@ -530,7 +530,7 @@ class mod_activequiz_renderer extends plugin_renderer_base
 
 
         $output .=  html_writer::start_tag('div', array('class' => 'dropdown-content'));
-        $output .=  html_writer::tag('button', "none", array( 'class' => 'btn',
+        $output .=  html_writer::tag('button', "show", array( 'class' => 'btn',
             'id' => 'show_chart_hide',
             'onclick' => 'activequiz.show_chart_hide();'));
 
