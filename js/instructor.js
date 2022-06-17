92c63697fea61a7470d94f31113d6a2682f9b101
Momo <immxmmi@gmx.at>
2022-06-17 09:49:16 +0200

    fix

--- js/instructor.js
+++ js/instructor.js
@@ -790,20 +790,60 @@ activequiz.show_chart_bar = function () {
 
 
     // CHART
-    var $apiChart = document.getElementById('apiChart');
+    var apiChart = document.getElementById('apiChart');
 
     //create a drawing context on the canvas
-    var ctx = $apiChart.getContext("2d");
+    var ctx = apiChart.getContext("2d");
 
 
 
     //declare variables
-    var myChart;
     var data;
 
     var url = './chart/chart_api.php';
     var chartTyp = 'bar'
 
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+
+
+
+
+    /*
     $(document).ready(function (){
     $.ajax({
             url: '' + url + '?sessionid=' + activequiz.get('sessionid') + '&type=' + chartTyp + '',
@@ -820,7 +860,7 @@ activequiz.show_chart_bar = function () {
             })
         });
     });
-
+*/
 
 
 
