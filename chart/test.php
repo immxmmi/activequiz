dcc98be916704db9b0d2bf3d9e1854a386f538b5
Momo <76620405+immxmmi@users.noreply.github.com>
2022-10-17 04:17:19 +0200

    add test area

--- chart/test.php
+++ chart/test.php
@@ -47,6 +47,17 @@ print_r($ListOfAllquestion);
 //var_dump($q->getQuestionsummary());
 //echo"<pre>";
 
-$question= new mdl_question($sessionid);
+//$question= new mdl_question($sessionid);
 
-var_dump($question);
+$id = 2;
+global $DB;
+        $sql = 'SELECT * FROM "public"."mdl_question_attempt_steps" WHERE id = :id';
+        $params = array('id' => $id);
+        $result = $DB->get_records_sql($sql, $params);
+
+        var_dump($result);
+
+
+
+
+
