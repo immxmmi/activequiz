edf54666ff9a96ea7bc7dc4d041af0df81082dee
Momo <76620405+immxmmi@users.noreply.github.com>
2022-10-17 03:31:04 +0200

    add question class

--- chart/test.php
+++ chart/test.php
@@ -10,16 +10,7 @@ require_once("class/single_choice.php");
 require_once("class/truefalsechoice.php");
 global $DB;
 
-// Parameter
-//$charttype = optional_param('type', false, PARAM_TEXT); //
-//$sessionid = optional_param('sessionid', false, PARAM_TEXT); //$sessionID = 46;
-$sessionid = 2; //$sessionID = 46;
-$charttype = "bar"; //
-$chart = new chart_builder();
 
-# # # # # # # # -SESSION- # # # # # # # #
-$session = new activequiz_session($sessionid);
-##########################################
 
 # # # # # # # #  -ACTIVE-QUIZ ATTEMPTS- # # # # # # # #
 $activequiz_attempt = new activequiz_attempt($sessionid);
@@ -28,66 +19,11 @@ $allquestionengids = $activequiz_attempt->getAllQuestionengids();
 
 
 
-
-
-
-# # # # # # # #  -QUESTION ATTEMPTS- # # # # # # # #
-$slot = $session->getCurrentquestion(); // SLOT
-// $slot = optional_param('slot', false, PARAM_TEXT); //; // SLOT
-$question_attemp = new question_attempts($allquestionengids, $slot);
-#####################################################
-
-
-
-
-
-$answers = $question_attemp->getListOfAnswers();
-
-$questionID = $question_attemp->getQuestionid();
-
-var_dump($answers);
-
-echo '<pre>'; var_dump($questionID); echo '</pre>';
+echo '<pre>'; var_dump($allquestionengids); echo '</pre>';
 echo "<br>";
 echo "<br>";
 echo "<br>";
-print_r($questionID);
+print_r($allquestionengids);
 
 
-
-$steps = new attempt_steps($answers);
-$steps = $steps->getAttemptstepids();
-
-$steps_data = new attempt_step_data($steps);
-$steps_data = $steps_data->getStepDataList();
-
-
-
-/*
-$questionType = "singel";
-$single = new single_choice();
-$trueFalse = new true_false_choice();
-$data = null;
-
-switch ($questionType) {
-    case "singel":
-        $single->load_quiz_data($answers,$steps_data);
-        $data = $chart->build_new_chart($charttype, $single->getLabels(), $single->getValues());
-        break;
-    case "true/false":
-        $trueFalse->setData($answers[0]);
-        $data = $chart->build_new_chart($charttype, $single->getLabels(), $single->getValues());
-    default:
-        $chart->setInfo("no Question Type Found!");
-        $data = $chart->build_new_chart(null, null, null);
-}
-
-http_response_code($chart->getResponseCode());
-header('Content-Type: application/json');
-
-echo json_encode($data, JSON_PRETTY_PRINT);
-exit;
-
-
-*/
 
