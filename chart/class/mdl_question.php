203b73bab206a9ab6a7acfb872133475ad8cf24e
Momo <76620405+immxmmi@users.noreply.github.com>
2022-10-17 04:18:38 +0200

    add test area

--- chart/class/mdl_question.php
+++ chart/class/mdl_question.php
@@ -15,7 +15,7 @@ class mdl_question
         if ($questionId !== null) {
 
             foreach ($questionId as $id) {
-                $sql = 'SELECT * FROM "public"."mdl_question_attempt_steps" WHERE id = :id';
+                $sql = 'SELECT * FROM "public"."mdl_question" WHERE id = :id';
                 $params = array('id' => $id);
                 $result = $DB->get_records_sql($sql, $params);
                 $this->id = $result->id;
