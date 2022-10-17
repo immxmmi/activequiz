f81678fd568b612bf1da6a0b545a248a0f49e053
Momo <76620405+immxmmi@users.noreply.github.com>
2022-10-17 02:47:42 +0200

    add test area

--- chart/create_pdf.php
+++ chart/create_pdf.php
@@ -3,7 +3,6 @@
     <meta charset="utf-8" />
     <script src="https://unpkg.com/pdf-lib@1.4.0"></script>
     <script src="https://unpkg.com/downloadjs@1.4.7"></script>
-    <script src="../js/pdf_generator.js"></script>
 </head>
 
 <body>
@@ -12,5 +11,6 @@
 <p class="small">(Your browser will download the resulting file)</p>
 </body>
 
+    <script src="../js/pdf_generator.js"></script>
 
 </html>
