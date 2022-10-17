fed811337cc49b89e21e0de95f941863a29cb71f
Momo <76620405+immxmmi@users.noreply.github.com>
2022-10-17 04:47:03 +0200

    add test area

---
+++ chart/class/quiz_builder.php
@@ -0,0 +1,72 @@
+<?php
+require_once("../../../config.php");
+require_once("mdl_question_attempts.php");
+
+class quiz_builder
+{
+    private $response_code = 200;
+    private $status = 'success';
+    private $msg = 'Chartdata successfully fetched';
+    private $data = array();
+    private $options = array();
+    private $info = '-';
+
+    public function __construct()
+    {
+    }
+
+
+    public function build_quiz_data($question){
+                $this->data = array(
+                    'question' => $question
+                );
+    }
+
+
+    private function convert_quiz_to_json()
+    {
+        http_response_code($this->response_code);
+        //header('Content-Type: application/json');
+        $response = array(
+            'meta' => array(
+                'status' => $this->status,
+                'msg' => $this->msg,
+                'info' => $this->info
+            ),
+            'data' => array(
+                'data' => $this->data,
+            )
+        );
+        return $response;
+    }
+
+
+
+
+    /**
+     * @return int
+     */
+    public function getResponseCode()
+    {
+        return $this->response_code;
+    }
+
+    /**
+     * @param string $msg
+     */
+    public function setMsg($msg)
+    {
+        $this->msg = $msg;
+    }
+
+    /**
+     * @param string $info
+     */
+    public function setInfo($info)
+    {
+        $this->info = $info;
+    }
+
+
+
+}
--- chart/quiz_api.php
+++ chart/quiz_api.php
@@ -2,20 +2,22 @@
 require_once("../../../config.php");
 require_once("class/mdl_activequiz_sessions.php");
 require_once("class/mdl_activequiz_attempt.php");
-require_once("class/mdl_question.php");
 require_once("class/mdl_question_attempts.php");
 require_once("class/mdl_question_attempt_steps.php");
 require_once("class/mdl_question_attempt_step_data.php");
-require_once("class/chart_builder.php");
+require_once("class/quiz_builder.php");
 require_once("class/single_choice.php");
 require_once("class/truefalsechoice.php");
 global $DB;
 
 // Parameter
-//$charttype = optional_param('type', false, PARAM_TEXT); //
-//$sessionid = optional_param('sessionid', false, PARAM_TEXT); //$sessionID = 46;
-$sessionid = 2; //$sessionID = 46;
-$charttype = "bar"; //
+$charttype = optional_param('type', false, PARAM_TEXT); //
+$sessionid = optional_param('sessionid', false, PARAM_TEXT); //$sessionID = 46;
+$quiz = new quiz_builder();
+
+# # # # # # # # -SESSION- # # # # # # # #
+$session = new activequiz_session($sessionid);
+##########################################
 
 # # # # # # # #  -ACTIVE-QUIZ ATTEMPTS- # # # # # # # #
 $activequiz_attempt = new activequiz_attempt($sessionid);
@@ -23,37 +25,44 @@ $allquestionengids = $activequiz_attempt->getAllQuestionengids();
 #######################################################
 
 # # # # # # # #  -QUESTION ATTEMPTS- # # # # # # # #
-$slot = 1;//$session->getCurrentquestion(); // SLOT
+$slot = $session->getCurrentquestion(); // SLOT
 // $slot = optional_param('slot', false, PARAM_TEXT); //; // SLOT
 $question_attemp = new question_attempts($allquestionengids, $slot);
 #####################################################
 
-$answers = $question_attemp->getListOfAnswers()[0];
+$answers = $question_attemp->getListOfAnswers();
 
-$ListOfAllquestion = [];
+$steps = new attempt_steps($answers);
+$steps = $steps->getAttemptstepids();
 
-
-foreach ( $allquestionengids as $questionID){
-    array_push( $ListOfAllquestion, $questionID);
-}
-
-//print_r($ListOfAllquestion);
+$steps_data = new attempt_step_data($steps);
+$steps_data = $steps_data->getStepDataList();
 
 
 
-//echo"<pre>";
-//foreach ($answers as $q)
-//var_dump($q->getQuestionID());
-//var_dump($q->getQuestionsummary());
-//echo"<pre>";
 
-$question= new mdl_question(2);
+$questionType = "singel";
+$single = new single_choice();
+$trueFalse = new true_false_choice();
+$data = null;
 
-echo "<pre>";
-var_dump($question);
-echo "</pre>";
+$data = $quiz->build_quiz_data("test");
 
 
+
+
+
+
+
+
+
+
+
+http_response_code($quiz->getResponseCode());
+header('Content-Type: application/json');
+
+echo json_encode($data, JSON_PRETTY_PRINT);
+exit;
 
 
 
