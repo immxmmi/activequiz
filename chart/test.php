d8ca0e953934022c5338b304cf18011fc5e17b64
Momo <76620405+immxmmi@users.noreply.github.com>
2022-10-17 04:18:11 +0200

    add test area

--- chart/test.php
+++ chart/test.php
@@ -51,7 +51,7 @@ print_r($ListOfAllquestion);
 
 $id = 2;
 global $DB;
-        $sql = 'SELECT * FROM "public"."mdl_question_attempt_steps" WHERE id = :id';
+        $sql = 'SELECT * FROM "public"."mdl_question" WHERE id = :id';
         $params = array('id' => $id);
         $result = $DB->get_records_sql($sql, $params);
 
