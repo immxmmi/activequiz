8481c34169f80374400f9f221713101b84c6585d
Momo <76620405+immxmmi@users.noreply.github.com>
2022-10-17 13:44:26 +0200

    test

--- chart/quiz_api.php
+++ chart/quiz_api.php
@@ -12,57 +12,12 @@ global $DB;
 
 // Parameter
 $charttype = optional_param('type', false, PARAM_TEXT); //
-$sessionid = optional_param('sessionid', false, PARAM_TEXT); //$sessionID = 46;
-$chart = new chart_builder();
+//$sessionid = optional_param('sessionid', false, PARAM_TEXT); //$sessionID = 46;
+//$chart = new chart_builder();
 
-# # # # # # # # -SESSION- # # # # # # # #
-$session = new activequiz_session($sessionid);
-##########################################
+echo json_decode($charttype, JSON_PRETTY_PRINT);
 
-# # # # # # # #  -ACTIVE-QUIZ ATTEMPTS- # # # # # # # #
-$activequiz_attempt = new activequiz_attempt($sessionid);
-$allquestionengids = $activequiz_attempt->getAllQuestionengids();
-#######################################################
-
-# # # # # # # #  -QUESTION ATTEMPTS- # # # # # # # #
-$slot = $session->getCurrentquestion(); // SLOT
-// $slot = optional_param('slot', false, PARAM_TEXT); //; // SLOT
-$question_attemp = new question_attempts($allquestionengids, $slot);
-#####################################################
-
-$answers = $question_attemp->getListOfAnswers();
-
-$steps = new attempt_steps($answers);
-$steps = $steps->getAttemptstepids();
-
-$steps_data = new attempt_step_data($steps);
-$steps_data = $steps_data->getStepDataList();
-
-
-
-
-$questionType = "singel";
-$single = new single_choice();
-$trueFalse = new true_false_choice();
-$data = null;
-
-switch ($questionType) {
-    case "singel":
-        $single->load_quiz_data($answers,$steps_data);
-        $data = $chart->build_new_chart($charttype, $single->getLabels(), $single->getValues());
-        break;
-    case "true/false":
-        $trueFalse->setData($answers[0]);
-        $data = $chart->build_new_chart($charttype, $single->getLabels(), $single->getValues());
-    default:
-        $chart->setInfo("no Question Type Found!");
-        $data = $chart->build_new_chart(null, null, null);
-}
-
-http_response_code($chart->getResponseCode());
-header('Content-Type: application/json');
-
-echo json_encode($data, JSON_PRETTY_PRINT);
+//echo json_encode($data, JSON_PRETTY_PRINT);
 exit;
 
 
