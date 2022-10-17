c9b0e97710fa6953ef683f6c8df141389b8f79ec
Momo <76620405+immxmmi@users.noreply.github.com>
2022-10-17 02:30:48 +0200

    add test area

--- chart/create_pdf.php
+++ chart/create_pdf.php
@@ -12,13 +12,15 @@ require_once("../../../config.php");
         }
     </style>
     <script src="<?php echo $CFG->wwwroot; ?>/lib/jquery/jquery-3.5.1.min.js"></script>
-    <script src="../js/pdf_generator.js"></script>
+    <script src="https://unpkg.com/pdf-lib@1.11.0"></script>
+    <script src="https://unpkg.com/downloadjs@1.4.7"></script>
 </head>
 
 <body>
 <p>Click the button to create a PDF document with form fields using <code>pdf-lib</code></p>
 <button onclick="createForm()">Create PDF</button>
 <p class="small">(Your browser will download the resulting file)</p>
+    <script src="../js/pdf_generator.js"></script>
 </body>
 
 </html>
