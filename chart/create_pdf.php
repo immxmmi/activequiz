e582e8b2f75df454109beefea7083b0b40168f65
Momo <76620405+immxmmi@users.noreply.github.com>
2022-10-17 02:42:31 +0200

    add test area

--- chart/create_pdf.php
+++ chart/create_pdf.php
@@ -12,44 +12,12 @@ require_once("../../../config.php");
         }
     </style>
     <script src="<?php echo $CFG->wwwroot; ?>/lib/jquery/jquery-3.5.1.min.js"></script>
-
-<script>
-    const { PDFDocument, StandardFonts, rgb } = PDFLib
-
-    async function createPdf() {
-        // Create a new PDFDocument
-        const pdfDoc = await PDFDocument.create()
-
-        // Embed the Times Roman font
-        const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
-
-        // Add a blank page to the document
-        const page = pdfDoc.addPage()
-
-        // Get the width and height of the page
-        const { width, height } = page.getSize()
-
-        // Draw a string of text toward the top of the page
-        const fontSize = 30
-        page.drawText('Creating PDFs in JavaScript is awesome!', {
-            x: 50,
-            y: height - 4 * fontSize,
-            size: fontSize,
-            font: timesRomanFont,
-            color: rgb(0, 0.53, 0.71),
-        })
-
-        // Serialize the PDFDocument to bytes (a Uint8Array)
-        const pdfBytes = await pdfDoc.save()
-
-        // Trigger the browser to download the PDF document
-        download(pdfBytes, "pdf-lib_creation_example.pdf", "application/pdf");
-    }
-</script>
+</head>
 
 <body>
 <button onclick="createPdf()">Create PDF</button>
 
+    <script src="../js/pdf_generator.js"></script>
 
 
 
