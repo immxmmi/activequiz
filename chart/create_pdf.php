c01f00b8271c1f53b3c65f72518c9f212289b3ba
Momo <76620405+immxmmi@users.noreply.github.com>
2022-10-17 02:32:42 +0200

    add test area

--- chart/create_pdf.php
+++ chart/create_pdf.php
@@ -18,9 +18,46 @@ require_once("../../../config.php");
 
 <body>
 <p>Click the button to create a PDF document with form fields using <code>pdf-lib</code></p>
-<button onclick="createForm()">Create PDF</button>
+<button onclick="createPdf()">Create PDF</button>
 <p class="small">(Your browser will download the resulting file)</p>
-    <script src="../js/pdf_generator.js"></script>
+
+
+
+<script>
+    const { PDFDocument, StandardFonts, rgb } = PDFLib
+
+    async function createPdf() {
+        // Create a new PDFDocument
+        const pdfDoc = await PDFDocument.create()
+
+        // Embed the Times Roman font
+        const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+
+        // Add a blank page to the document
+        const page = pdfDoc.addPage()
+
+        // Get the width and height of the page
+        const { width, height } = page.getSize()
+
+        // Draw a string of text toward the top of the page
+        const fontSize = 30
+        page.drawText('Creating PDFs in JavaScript is awesome!', {
+            x: 50,
+            y: height - 4 * fontSize,
+            size: fontSize,
+            font: timesRomanFont,
+            color: rgb(0, 0.53, 0.71),
+        })
+
+        // Serialize the PDFDocument to bytes (a Uint8Array)
+        const pdfBytes = await pdfDoc.save()
+
+        // Trigger the browser to download the PDF document
+        download(pdfBytes, "pdf-lib_creation_example.pdf", "application/pdf");
+    }
+</script>
+
+
 </body>
 
 </html>
