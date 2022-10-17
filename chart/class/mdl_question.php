70c7f88cc49dc4d3a92c50ec76ee3374d979581a
Momo <76620405+immxmmi@users.noreply.github.com>
2022-10-17 13:56:19 +0200

    test

---
+++ chart/class/mdl_question.php
@@ -0,0 +1,36 @@
+<?php
+require_once("../../../config.php");
+global $DB;
+class mdl_question
+{
+    private $id;
+    private $category;
+    private $name;
+    private $questiontext;
+
+
+    public function __construct($id)
+    {
+        global $DB;
+        if ($id !== null) {
+
+                $sql = 'SELECT * FROM "public"."mdl_question" WHERE id = :id';
+                $params = array('id' => $id);
+                $result = $DB->get_records_sql($sql, $params);
+
+                foreach ($result as $answer){
+                    $this->id = $answer->id;
+                    $this->category = $answer->category;
+                    $this->name=$answer->name;
+                    $this->questiontext=$answer->questiontext;
+                }
+
+        }
+
+    }
+
+
+
+
+}
+
