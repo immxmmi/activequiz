74b47f8a9c1d345366ef3cd1b90cf4572fc2c374
Momo <76620405+immxmmi@users.noreply.github.com>
2022-10-19 09:38:02 +0200

    add quiz data

--- chart/class/quiz_builder.php
+++ chart/class/quiz_builder.php
@@ -19,7 +19,7 @@ class quiz_builder
     public function build_quiz_data($question, $answer){
                 $this->data = array(
                     'question' => $question,
-                    'answers' => $answer
+                    'answers' => $answer[0]
                 );
         return $this->convert_quiz_to_json();
     }
