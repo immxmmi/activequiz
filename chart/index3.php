2ec16660fdb68f54156b71e3a538f64700842e52
Momo <76620405+immxmmi@users.noreply.github.com>
2022-10-19 21:57:06 +0200

    pdf index3

--- chart/index3.php
+++ chart/index3.php
@@ -22,7 +22,7 @@ require_once("../../../config.php");
     var session = 2;
     /// QUIZ DATA
     var quizdata = null;
-    if (session != null) {
+    if (session !== null) {
         var url = './quiz_api.php';
         var params = {
             sessionid: session
@@ -41,33 +41,37 @@ require_once("../../../config.php");
     const {PDFDocument, StandardFonts, rgb} = PDFLib
 
     async function createPdf() {
-        // Create a new PDFDocument
-        const pdfDoc = await PDFDocument.create()
+            console.log(data);
+            console.log(data.meta);
+        if (data === null) {
+            // Create a new PDFDocument
+            const pdfDoc = await PDFDocument.create()
 
-        // Embed the Times Roman font
-        const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+            // Embed the Times Roman font
+            const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
 
-        // Add a blank page to the document
-        const page = pdfDoc.addPage()
+            // Add a blank page to the document
+            const page = pdfDoc.addPage()
 
-        // Get the width and height of the page
-        const {width, height} = page.getSize()
+            // Get the width and height of the page
+            const {width, height} = page.getSize()
 
-        // Draw a string of text toward the top of the page
-        const fontSize = 30
-        page.drawText('Creating PDFs in JavaScript is awesome!', {
-            x: 50,
-            y: height - 4 * fontSize,
-            size: fontSize,
-            font: timesRomanFont,
-            color: rgb(0, 0.53, 0.71),
-        })
+            // Draw a string of text toward the top of the page
+            const fontSize = 30
+            page.drawText('Creating PDFs in JavaScript is awesome!', {
+                x: 50,
+                y: height - 4 * fontSize,
+                size: fontSize,
+                font: timesRomanFont,
+                color: rgb(0, 0.53, 0.71),
+            })
 
-        // Serialize the PDFDocument to bytes (a Uint8Array)
-        const pdfBytes = await pdfDoc.save()
+            // Serialize the PDFDocument to bytes (a Uint8Array)
+            const pdfBytes = await pdfDoc.save()
 
-        // Trigger the browser to download the PDF document
-        download(pdfBytes, "pdf-lib_creation_example.pdf", "application/pdf");
+            // Trigger the browser to download the PDF document
+            download(pdfBytes, "pdf-lib_creation_example.pdf", "application/pdf");
+        }
     }
 </script>
 </html>
