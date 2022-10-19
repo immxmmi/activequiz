6d9c0202282e72ff499cbd779e86619c98c198e1
Momo <76620405+immxmmi@users.noreply.github.com>
2022-10-19 22:01:20 +0200

    pdf index3

--- chart/index3.php
+++ chart/index3.php
@@ -41,7 +41,7 @@ require_once("../../../config.php");
     const {PDFDocument, StandardFonts, rgb} = PDFLib
 
     async function createPdf() {
-            var qu = quizdata.data.data.questions;
+            var qu = quizdata.data.data;
       console.log(qu);
 
         if (quizdata === null) {
