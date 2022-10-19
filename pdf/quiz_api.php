02b8cabb6c79e43715dcdc00f522dfda1d566c28
Momo <76620405+immxmmi@users.noreply.github.com>
2022-10-19 23:20:54 +0200

    clean code

--- pdf/quiz_api.php
+++ pdf/quiz_api.php
@@ -1,6 +1,6 @@
 <?php
 require_once("../../../config.php");
-require_once("../chart/class/mdl_question_data.php");
+require_once("./class/mdl_question_data.php");
 require_once("../chart/class/mdl_activequiz_attempt.php");
 require_once("../chart/class/mdl_activequiz_sessions.php");
 require_once("../chart/class/mdl_question_attempts.php");
