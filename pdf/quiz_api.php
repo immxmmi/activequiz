2f69c4872d70239ea0aef2e231c3a3c5ae2a60f1
Momo <76620405+immxmmi@users.noreply.github.com>
2022-10-19 21:16:16 +0200

    add quiz data

--- pdf/quiz_api.php
+++ pdf/quiz_api.php
@@ -1,31 +1,63 @@
 <?php
 require_once("../../../config.php");
-require_once("chart/class/mdl_activequiz_sessions.php");
-require_once("chart/class/mdl_activequiz_attempt.php");
-require_once("chart/class/mdl_question_attempts.php");
-require_once("chart/class/mdl_question_attempt_steps.php");
-require_once("chart/class/mdl_question_attempt_step_data.php");
-require_once("chart/class/quiz_builder.php");
-require_once("chart/class/single_choice.php");
-require_once("chart/class/truefalsechoice.php");
+require_once("class/mdl_activequiz_sessions.php");
+require_once("class/mdl_activequiz_attempt.php");
+require_once("class/mdl_question_attempts.php");
+require_once("class/mdl_question.php");
+require_once("class/mdl_question_data.php");
+require_once("class/mdl_question_attempt_steps.php");
+require_once("class/mdl_question_attempt_step_data.php");
+require_once("class/chart_builder.php");
+require_once("class/quiz_builder.php");
+require_once("class/single_choice.php");
+require_once("class/truefalsechoice.php");
 global $DB;
 
+
+$quiz_build = new quiz_builder();
 // Parameter
+//$questuinid = optional_param('questionid', PARAM_TEXT);
+$sessionid = optional_param('sessionid', false, PARAM_TEXT); //$sessionID = 46;
 
-//$question = optional_param('question', false, PARAM_TEXT); //
-$quiz = new quiz_builder();
+//QUIZ BUILDER
+//$quiz = new quiz_builder();
+# # # # # # # #  -ACTIVE-QUIZ ATTEMPTS- # # # # # # # #
+$activequiz_attempt = new activequiz_attempt($sessionid);
+$allquestionengids = $activequiz_attempt->getAllQuestionengids();
+#######################################################
 
 
-$data = null;
-$quiz->setInfo("no Question Type Found!");
-$quiz = $quiz->build_quiz_data("test");
+//$summary = new question_attempts(3,1);
+
+//$question= new mdl_question(2);
+
+if ($allquestionengids != null) {
+
+    $quizdata = array();
+
+    foreach ($allquestionengids as $id) {
+        array_push($quizdata, new question_data($id));
+    }
+}
+
+$qu = array();
+$aw = array();
 
 
-http_response_code(200);
+foreach ($quizdata as $qd) {
+    array_push($qu, $qd->getQuestion());
+    array_push($aw, $qd->getAnswers());
+}
+$data = $quiz_build->build_quiz_data($qu, $aw);
+
+http_response_code($quiz_build->getResponseCode());
 header('Content-Type: application/json');
 
 echo json_encode($data, JSON_PRETTY_PRINT);
 exit;
+
+
+
 
 
 
