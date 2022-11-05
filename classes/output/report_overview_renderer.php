b995a3203b2dcd81efd7922570957132a8e0b3a8
Momo <76620405+immxmmi@users.noreply.github.com>
2022-11-05 10:59:28 +0100

    Modify PDF-Button in Report

--- classes/output/report_overview_renderer.php
+++ classes/output/report_overview_renderer.php
@@ -70,7 +70,7 @@ class report_overview_renderer extends \plugin_renderer_base {
         $selectsession .= \html_writer::div($this->output->render($sessionselect), 'inline-block');
         $selectsession .= \html_writer::end_div();
 
-        $selectsession .= \html_writer::tag('button', 'PDF Download', array('id' => 'printPfd', 'type' => 'submit' , 'class' => 'btn btn-info', 'onclick' => $this->test()));
+        $selectsession .= \html_writer::tag('button', 'PDF Download', array('id' => 'printPfd', 'type' => 'submit' , 'class' => 'btn btn-info', 'onclick' => ''+$this->test()));
 
         $output .= $selectsession;
 
