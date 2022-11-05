3c3b3c29e07154f3aa504ce8ab02d8f05799d92d
Momo <76620405+immxmmi@users.noreply.github.com>
2022-11-05 10:32:59 +0100

    Modify PDF-Button in Report

--- classes/output/report_overview_renderer.php
+++ classes/output/report_overview_renderer.php
@@ -45,7 +45,7 @@ class report_overview_renderer extends \plugin_renderer_base {
         $selectsession = \html_writer::start_div('');
         $selectsession .= \html_writer::tag('h3', get_string('selectsession', 'activequiz'), array('class' => 'inline-block'));
         $selectsession .= \html_writer::tag('h3', $selectedid);
-        $selectsession .= \html_writer::tag('button', 'pdfButton', array('id' => 'printPfd'));
+        $selectsession .= \html_writer::tag('button', 'PDF Download', array('id' => 'printPfd', 'class' => 'pdfButton'));
 
 
         $sessionselecturl = clone($this->pageurl);
