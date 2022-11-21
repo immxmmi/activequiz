c8369e35271adb0159591253ca1649b9f9a6cd84
Momo <76620405+immxmmi@users.noreply.github.com>
2022-11-21 10:08:44 +0100

    new function

--- backend/pdf_index.php
+++ backend/pdf_index.php
@@ -9,10 +9,6 @@ require_once("../../../config.php");
     <script src="<?php echo $CFG->wwwroot; ?>/lib/jquery/jquery-3.5.1.min.js"></script>
     <script src="https://unpkg.com/pdf-lib@1.4.0"></script>
     <script src="https://unpkg.com/downloadjs@1.4.7"></script>
-    <script src="<?php echo $CFG->wwwroot; ?>/lib/jquery/jquery-3.5.1.min.js"></script>
-    <script src="<?php echo $CFG->wwwroot; ?>/mod/activequiz/js/chart_js/Chart.min.js"></script>
-    <script src="../js/chart_js_api.js"></script>
-
 </head>
 
 <body>
