f18c9e2c09af6b63a3c1fdb729f071dd6ece7c7f
Momo <76620405+immxmmi@users.noreply.github.com>
2022-11-21 14:38:17 +0100

    new function

--- classes/output/report_overview_renderer.php
+++ classes/output/report_overview_renderer.php
@@ -71,9 +71,9 @@ class report_overview_renderer extends \plugin_renderer_base
         $selectsession .= \html_writer::div($this->output->render($sessionselect), 'inline-block');
         $selectsession .= \html_writer::end_div();
 
+        $selectsession .= \html_writer::tag('script', '', array('src' => 'https://unpkg.com/pdf-lib'));
         $selectsession .= \html_writer::tag('button', 'PDF Download', array('id' => 'printPfd', 'type' => 'submit', 'class' => 'btn btn-info', 'OnClick' => 'createPdf('.$selectedid.')'));
         $selectsession .= \html_writer::tag('script', '', array('src' => './js/pdf_generator.js'));
-        $selectsession .= \html_writer::tag('script', '', array('src' => 'https://unpkg.com/pdf-lib'));
         $output .= $selectsession;
 
         $regradeurl = clone($this->pageurl);
