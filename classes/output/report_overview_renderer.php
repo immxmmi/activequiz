3a08a943810a579599770440f2fe24fe6f27410c
Momo <76620405+immxmmi@users.noreply.github.com>
2022-11-21 14:32:13 +0100

    new function

--- classes/output/report_overview_renderer.php
+++ classes/output/report_overview_renderer.php
@@ -71,8 +71,8 @@ class report_overview_renderer extends \plugin_renderer_base
         $selectsession .= \html_writer::div($this->output->render($sessionselect), 'inline-block');
         $selectsession .= \html_writer::end_div();
 
-        $selectsession .= \html_writer::tag('script', '', array('src' => './js/pdf_generator.js'));
         $selectsession .= \html_writer::tag('button', 'PDF Download', array('id' => 'printPfd', 'type' => 'submit', 'class' => 'btn btn-info', 'OnClick' => 'createPdf('.$selectedid.')'));
+        $selectsession .= \html_writer::tag('script', '', array('src' => './js/pdf_generator.js'));
         $output .= $selectsession;
 
         $regradeurl = clone($this->pageurl);
