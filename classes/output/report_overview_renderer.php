8959cffdb63e2e2bb5bfad64d85b7dd0774cdf66
Momo <76620405+immxmmi@users.noreply.github.com>
2022-11-21 13:23:34 +0100

    new function

--- classes/output/report_overview_renderer.php
+++ classes/output/report_overview_renderer.php
@@ -71,6 +71,7 @@ class report_overview_renderer extends \plugin_renderer_base
         $selectsession .= \html_writer::end_div();
 
         $selectsession .= \html_writer::tag('button', 'PDF Download', array('id' => 'printPfd', 'type' => 'submit', 'class' => 'btn btn-info', 'OnClick' => '(function(){alert("PDF DOWANLOAD OF ID '.$selectedid.' ");return false;})();return false;'));
+        $selectsession .= \html_writer::tag('button', 'PDF Test', array('id' => 'printme', 'type' => 'submit', 'class' => 'btn btn-info', 'OnClick' => 'createPdf()'));
 
         $output .= $selectsession;
 
