41beb0f4e228b107db0b43d099fddf2e3ebdc4c0
Momo <76620405+immxmmi@users.noreply.github.com>
2022-11-21 10:30:02 +0100

    new function

--- backend/pdf_index.php
+++ backend/pdf_index.php
@@ -46,8 +46,7 @@ require_once("../../../config.php");
 
     const {PDFDocument, StandardFonts, rgb} = PDFLib
     async function generateChartBySessionAndSlot(sessionid,slot){
-        $link = 'https://www.moodle.local/mod/activequiz/backend/api/chart_api.php?sessionid=11&type=bar&slot=';
-        $link =. slot;
+        $link = 'https://www.moodle.local/mod/activequiz/backend/api/chart_api.php?sessionid=11&type=bar&slot='+slot;
         console.log($link)
         return    $.getJSON('https://www.moodle.local/mod/activequiz/backend/api/chart_api.php?sessionid=11&type=bar&slot=3');
     }
