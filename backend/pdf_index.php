2a0a0b9e7eeb5ebeeddf4601ac67497be236ab7a
Momo <76620405+immxmmi@users.noreply.github.com>
2022-11-21 10:08:00 +0100

    new function

--- backend/pdf_index.php
+++ backend/pdf_index.php
@@ -23,7 +23,7 @@ require_once("../../../config.php");
 <div>
     <form action="javascript:void(0);">
         <label for="session">Session ID:</label>
-        <input type="number" id="sessionid" name="session" >
+        <input type="number" id="sessionid" name="session" value="2">
     </form>
 </div>
 
