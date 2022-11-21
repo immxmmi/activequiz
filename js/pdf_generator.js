0b67197858401f27d3198695c717b2060d9ea48e
Momo <76620405+immxmmi@users.noreply.github.com>
2022-11-21 11:21:36 +0100

    new function

--- js/pdf_generator.js
+++ js/pdf_generator.js
@@ -20,21 +20,25 @@ if (session !== null) {
 // QUIZ DATA
 const {PDFDocument, StandardFonts, rgb} = PDFLib
 
-var dano = null;
+let dano = null;
 // Generate Chart By Parameter
 async function generateChartBySessionAndSlot(sessionid, type, slot) {
+    let data = null;
     $.getJSON('https://www.moodle.local/mod/activequiz/backend/api/chart_api.php?sessionid=' + sessionid + '&type=' + type + '&slot=' + slot, function (data) {
          dano = data;
     });
+
+    console.log(dano);
    return dano;
 }
 
 async function createPdf(session) {
 
 
-    $data = generateChartBySessionAndSlot(session, 'bar', 3);
+    //var data =
+        generateChartBySessionAndSlot(session, 'bar', 3);
 
-    console.log(data);
+    console.log(dano);
 
 
     // TIME
