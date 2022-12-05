89afa6cc6f8d950280326fec0d127ebb09b2ef34
Momo <76620405+immxmmi@users.noreply.github.com>
2022-12-05 10:54:10 +0100

    fixed fetch

--- js/pdf_generator.js
+++ js/pdf_generator.js
@@ -4,7 +4,7 @@ const {PDFDocument, StandardFonts, rgb} = PDFLib
 let quizdata = [];
 
 // Generate Chart By Parameter
-function generateChartBySessionAndSlot(sessionid, type, slot) {
+async function generateChartBySessionAndSlot(sessionid, type, slot) {
 
     var url = '/mod/activequiz/backend/api/chart_api.php/';
     var params = {sessionid: sessionid, type: type, slot: slot};
@@ -19,12 +19,12 @@ function cleanQuizdata(){
 }
 
 
-async function createPdf(sessionID) {
+function createPdf(sessionID) {
 
     // QUIZDATA
-    await generateChartBySessionAndSlot(sessionID, 'bar', 1);
-    await generateChartBySessionAndSlot(sessionID, 'pie', 1);
-    await generateChartBySessionAndSlot(sessionID, 'doughnut', 1);
+     generateChartBySessionAndSlot(sessionID, 'bar', 1);
+     generateChartBySessionAndSlot(sessionID, 'pie', 1);
+     generateChartBySessionAndSlot(sessionID, 'doughnut', 1);
 
     console.log(quizdata);
 
@@ -32,10 +32,10 @@ async function createPdf(sessionID) {
 
     // Test Data
     const question = ["Question 1", "Question 2"];
-    const answers = [["Answer1", "Answer2", "Answer3", "Answer4"],["Answer1", "Answer2", "Answer3", "Answer4"]];
+    const answers = [["Answer1", "Answer2", "Answer3", "Answer4"], ["Answer1", "Answer2", "Answer3", "Answer4"]];
     const rightAnswer = ["Answer1", "Answer2"];
 
-  //  cleanQuizdata();
+    //  cleanQuizdata();
     /*
 
     // TIME
@@ -48,95 +48,95 @@ async function createPdf(sessionID) {
     */
 
     // Deckblatt
-   //          const reportUrl = '/mod/activequiz/backend/assets/ActiveQuiz_Report_Deckblatt.pdf';
-   //          const existingPdfBytes = await fetch(reportUrl).then(res => res.arrayBuffer());
+    //          const reportUrl = '/mod/activequiz/backend/assets/ActiveQuiz_Report_Deckblatt.pdf';
+    //          const existingPdfBytes = await fetch(reportUrl).then(res => res.arrayBuffer());
 //
-   //          // Logo
-   //          const pngUrl = '/mod/activequiz/backend/assets/fh_logo.png';
-   //          const pngImageBytes = await fetch(pngUrl).then((res) => res.arrayBuffer());
+    //          // Logo
+    //          const pngUrl = '/mod/activequiz/backend/assets/fh_logo.png';
+    //          const pngImageBytes = await fetch(pngUrl).then((res) => res.arrayBuffer());
 //
-   //          // Chart
-   //          const chartUrl = '/mod/activequiz/backend/assets/Chart.png';
-   //          const chartImageBytes = await fetch(chartUrl).then((res) => res.arrayBuffer());
+    //          // Chart
+    //          const chartUrl = '/mod/activequiz/backend/assets/Chart.png';
+    //          const chartImageBytes = await fetch(chartUrl).then((res) => res.arrayBuffer());
 //
-   //          const pdfDoc = await PDFDocument.load(existingPdfBytes);
-   //          const pngImage = await pdfDoc.embedPng(pngImageBytes);
-   //          const chartImage = await pdfDoc.embedPng(chartImageBytes);
-//
-//
-//
-   //          const pages = pdfDoc.getPages();
-//
-   //          //const pdfDoc = await PDFLib.PDFDocument.create();
-   //          const arialFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+    //          const pdfDoc = await PDFDocument.load(existingPdfBytes);
+    //          const pngImage = await pdfDoc.embedPng(pngImageBytes);
+    //          const chartImage = await pdfDoc.embedPng(chartImageBytes);
 //
 //
-   //          const firstPage = pages[0];
-   //          const { width, height } = firstPage.getSize();
-   //          firstPage.drawText('Lektor: Testikus Testor', {
-   //              x: 50,
-   //              y: height/2,
-   //              size: 34,
-   //              font: arialFont,
-   //              color: rgb(0.0, 0.392, 0.612), //blau
-   //          });
 //
-   //          for(var i = 0; i < question.length; i++){
-   //              const page = pdfDoc.addPage();
-   //              page.drawImage(pngImage, {
-   //                  x: 10,
-   //                  y: height-126,
-   //                  width: 180,
-   //                  height: 113
-   //              });
-   //              page.drawText(question[i], {
-   //                  x: 40,
-   //                  y: height-126-30,
-   //                  size: 28,
-   //                  font: arialFont,
-   //                  color: rgb(0, 0.1, 0.156),
-   //                  maxWidth: width-80
-   //              });
-   //              const form = pdfDoc.getForm();
-   //              const radioGroup = form.createRadioGroup(question[i]);
+    //          const pages = pdfDoc.getPages();
 //
-   //              for(var j = 0; j < answers[i].length; j++){
-   //                  if(answers[i][j] == rightAnswer[i]){
-   //                      page.drawText(answers[i][j], {
-   //                          x: 70,
-   //                          y: height-126-30-40-(40*j),
-   //                          size: 18,
-   //                          font: arialFont,
-   //                          color: rgb(0.537, 0.702, 0.114),
-   //                      });
-   //                      radioGroup.addOptionToPage(answers[i][j], page, { height: 15, width: 15, x: 43,  y: height-126-30-40-(40*j) });
-   //                      radioGroup.select(answers[i][j]);
-   //                  }
-   //                  else{
-   //                      page.drawText(answers[i][j], {
-   //                          x: 70,
-   //                          y: height-126-30-40-(40*j),
-   //                          size: 18,
-   //                          font: arialFont,
-   //                          color: rgb(0, 0.1, 0.156),
-   //                      });
-   //                      radioGroup.addOptionToPage(answers[i][j], page, { height: 15, width: 15, x: 43,  y: height-126-30-40-(40*j) });
-   //                  }
-   //              }
-   //              page.drawImage(chartImage, {
-   //                  x: 30,
-   //                  y: height-126-30-40-(40*j)-300,
-   //                  width: 500,
-   //                  height: 300,
-   //              });
-   //              form.flatten();
-   //          }
-   //          const pdfBytes = await pdfDoc.save();
-   //          const d = new Date();
-   //          const time = d.getTime();
-   //          download(pdfBytes, "QUIZ PDF" + time.toString() , "application/pdf");
+    //          //const pdfDoc = await PDFLib.PDFDocument.create();
+    //          const arialFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
 //
-   //
+//
+    //          const firstPage = pages[0];
+    //          const { width, height } = firstPage.getSize();
+    //          firstPage.drawText('Lektor: Testikus Testor', {
+    //              x: 50,
+    //              y: height/2,
+    //              size: 34,
+    //              font: arialFont,
+    //              color: rgb(0.0, 0.392, 0.612), //blau
+    //          });
+//
+    //          for(var i = 0; i < question.length; i++){
+    //              const page = pdfDoc.addPage();
+    //              page.drawImage(pngImage, {
+    //                  x: 10,
+    //                  y: height-126,
+    //                  width: 180,
+    //                  height: 113
+    //              });
+    //              page.drawText(question[i], {
+    //                  x: 40,
+    //                  y: height-126-30,
+    //                  size: 28,
+    //                  font: arialFont,
+    //                  color: rgb(0, 0.1, 0.156),
+    //                  maxWidth: width-80
+    //              });
+    //              const form = pdfDoc.getForm();
+    //              const radioGroup = form.createRadioGroup(question[i]);
+//
+    //              for(var j = 0; j < answers[i].length; j++){
+    //                  if(answers[i][j] == rightAnswer[i]){
+    //                      page.drawText(answers[i][j], {
+    //                          x: 70,
+    //                          y: height-126-30-40-(40*j),
+    //                          size: 18,
+    //                          font: arialFont,
+    //                          color: rgb(0.537, 0.702, 0.114),
+    //                      });
+    //                      radioGroup.addOptionToPage(answers[i][j], page, { height: 15, width: 15, x: 43,  y: height-126-30-40-(40*j) });
+    //                      radioGroup.select(answers[i][j]);
+    //                  }
+    //                  else{
+    //                      page.drawText(answers[i][j], {
+    //                          x: 70,
+    //                          y: height-126-30-40-(40*j),
+    //                          size: 18,
+    //                          font: arialFont,
+    //                          color: rgb(0, 0.1, 0.156),
+    //                      });
+    //                      radioGroup.addOptionToPage(answers[i][j], page, { height: 15, width: 15, x: 43,  y: height-126-30-40-(40*j) });
+    //                  }
+    //              }
+    //              page.drawImage(chartImage, {
+    //                  x: 30,
+    //                  y: height-126-30-40-(40*j)-300,
+    //                  width: 500,
+    //                  height: 300,
+    //              });
+    //              form.flatten();
+    //          }
+    //          const pdfBytes = await pdfDoc.save();
+    //          const d = new Date();
+    //          const time = d.getTime();
+    //          download(pdfBytes, "QUIZ PDF" + time.toString() , "application/pdf");
+//
+    //
     //
     /*
 
