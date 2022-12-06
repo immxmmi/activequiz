f45b960a02e58ea4c0e3825a1fbd565b01aa23e5
Momo <76620405+immxmmi@users.noreply.github.com>
2022-12-06 20:26:14 +0100

    try #11

--- classes/output/report_overview_renderer.php
+++ classes/output/report_overview_renderer.php
@@ -76,6 +76,7 @@ class report_overview_renderer extends \plugin_renderer_base
         $selectsession .= \html_writer::tag('script', '', array('src' => 'https://unpkg.com/downloadjs@1.4.7'));
         $selectsession .= \html_writer::tag('button', 'PDF Download', array('id' => 'printPfd', 'type' => 'submit', 'class' => 'btn btn-info', 'OnClick' => 'createPdf('.$selectedid.')'));
         $selectsession .= \html_writer::tag('script', '', array('src' => './js/pdf_generator.js'));
+        $selectsession .= \html_writer::tag('script', '', array('src' => './js/chartdata/Chart.js'));
         $selectsession .= \html_writer::tag('canvis', '', array('id' => 'chart'));
         $output .= $selectsession;
 
