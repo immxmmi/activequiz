db120b9abc80eb1447b791e99f2cf324e07776c2
Momo <76620405+immxmmi@users.noreply.github.com>
2022-12-10 22:06:23 +0100

    Test Chart Img

--- js/pdf_generator.js
+++ js/pdf_generator.js
@@ -197,7 +197,7 @@ async function buildPdf() {
    //}
 
 
-    page.drawImage(chartImage, {
+    page.drawImage(chartIurlImg, {
                x: 30,
                y: 30,
                width: 500,
