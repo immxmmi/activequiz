d11ed269fa964e407ed97a39512b6b2950478b95
Momo <76620405+immxmmi@users.noreply.github.com>
2022-12-10 22:32:04 +0100

    Test Chart Img

--- js/pdf_generator.js
+++ js/pdf_generator.js
@@ -99,8 +99,8 @@ async function buildPdf(title,question, answers, rightAnswer, labels, chartType)
     const pngUrl = '/mod/activequiz/backend/assets/fh_logo.png';
     const pngImageBytes = await fetch(pngUrl).then((res) => res.arrayBuffer());
     // Chart
-    const lab = ''
-    const url = "https://quickchart.io/chart?c={type:'"+chartType+"',data:{labels:[2012,2013,2014,2015, 2016],datasets:[{label:'Users',data:[120,60,50,180,120]}]}}";
+    const lab = "[2012,2013,2014,2015, 2016]";
+    const url = "https://quickchart.io/chart?c={type:'"+chartType+"',data:{labels:"+[2012,2013,2014,2015, 2016]+",datasets:[{label:'Users',data:[120,60,50,180,120]}]}}";
     console.log(url);
 
     const chartImageBytes = await fetch(url).then((res) => res.arrayBuffer());
