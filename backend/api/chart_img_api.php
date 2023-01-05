6073b222393cfdeaaa1f9d247564be7a5392a37f
Momo <76620405+immxmmi@users.noreply.github.com>
2023-01-05 10:51:01 +0100

    TEST

--- backend/api/chart_img_api.php
+++ backend/api/chart_img_api.php
@@ -6,9 +6,6 @@ require_once ("../lib/jpgraph-4.4.1/src/jpgraph_bar.php");
 // PARAMETER
 
 // Create the graph. These two calls are always required
-$graph = new Graph(350,220,'auto');
-$graph->SetScale("textlin");
-
 
 $height = optional_param('height', false, PARAM_TEXT);
 if(!$height){
@@ -18,6 +15,12 @@ $weight = optional_param('weight', false, PARAM_TEXT);
 if(!$weight){
     $weight = 400;
 }
+
+$graph = new Graph($height,$weight,'auto');
+$graph->SetScale("textlin");
+
+
+
 
 $type = optional_param('type', false, PARAM_TEXT);
 $label = optional_param('label', false, PARAM_TEXT);
