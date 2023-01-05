1e1ab77daf509199c18d64eb39f1ffd59052ae1b
Momo <76620405+immxmmi@users.noreply.github.com>
2023-01-05 09:30:34 +0100

    TEST

--- backend/api/chart_img_api.php
+++ backend/api/chart_img_api.php
@@ -7,17 +7,16 @@ require_once("../class/mdl_question_attempts.php");
 require_once("../builder/chart_img_builder.php");
 global $DB;
 
-//https://quickchart.io/chart?c={
-//type:'bar',
-//data:{labels:['Q1','Q2','Q3','Q4'], datasets:[{label:'Users', data:[50,60,70,180]},{label:'Revenue',data:[100,200,300,400]}]}}
+//https://quickchart.io/chart?type:'bar'&=data:{labels:['Q1','Q2','Q3','Q4'], datasets:[{label:'Users', data:[50,60,70,180]},{label:'Revenue',data:[100,200,300,400]}]}
 
 
 
 // PARAMETER
+//$rowdata = optional_param('c', false, PARAM_TEXT);
 $type = optional_param('type', false, PARAM_TEXT);
-$c = optional_param('c', false, PARAM_TEXT);
 $data = optional_param('data', false, PARAM_TEXT);
-var_dump($c);
+var_dump($type);
+var_dump($data);
 //$img = new img();
 //http_response_code($img->getResponseCode());
 //header('Content-Type: application/json');
