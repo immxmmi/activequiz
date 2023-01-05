ddfd2af021df08a94bc32a967916ab91af50d1b2
Momo <76620405+immxmmi@users.noreply.github.com>
2023-01-05 08:56:06 +0100

    TEST

--- renderer.php
+++ renderer.php
@@ -526,7 +526,7 @@ class mod_activequiz_renderer extends plugin_renderer_base
         $inqcontrol .= html_writer::tag('button', "Show Chart",
             array(
                 'class' => 'btn',
-                'disabled' => 'true'
+                'enable' => 'true'
             ));
 
         $inqcontrol .= html_writer::start_tag('div', array('class' => 'dropdown-content'));
