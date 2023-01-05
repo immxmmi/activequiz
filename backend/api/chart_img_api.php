569d7bb1ccba8e81225f63db1841230a8abde79a
Momo <76620405+immxmmi@users.noreply.github.com>
2023-01-05 09:39:58 +0100

    TEST

--- backend/api/chart_img_api.php
+++ backend/api/chart_img_api.php
@@ -14,8 +14,12 @@ global $DB;
 // PARAMETER
 //$rowdata = optional_param('c', false, PARAM_TEXT);
 $type = optional_param('type', false, PARAM_TEXT);
+$labels = optional_param('data', false, PARAM_TEXT);
+$label = optional_param('data', false, PARAM_TEXT);
 $data = optional_param('data', false, PARAM_TEXT);
 var_dump($type);
+var_dump($labels);
+var_dump($label);
 var_dump($data);
 //$img = new img();
 //http_response_code($img->getResponseCode());
