7f8e74a6ce36eefff631b4a39a0cf428490387f9
Momo <76620405+immxmmi@users.noreply.github.com>
2023-01-05 11:06:42 +0100

    TEST

--- backend/api/chart_img_api.php
+++ backend/api/chart_img_api.php
@@ -9,27 +9,36 @@ global $DB;
 
 // PARAMETER
 $height = optional_param('height', false, PARAM_TEXT);
+if(!$height){$height = 250;}
+
 $weight = optional_param('weight', false, PARAM_TEXT);
-$type = optional_param('type', false, PARAM_TEXT);
-$labels = optional_param('labels', false, PARAM_TEXT);
-$label = optional_param('label', false, PARAM_TEXT);
-$data = optional_param('data', false, PARAM_TEXT);
-
-if(!$height){
-    $height = 250;
-}
-if(!$weight){
-    $weight = 350;
-}
-
-$datay=array(62,105,85,50);
-
-var_dump($data);
-var_dump($datay);
+if(!$weight){$weight = 350;}
 
 // Create the graph. These two calls are always required
 $graph = new Graph($weight,$height,'auto');
 $graph->SetScale("textlin");
+
+
+$type = optional_param('type', false, PARAM_TEXT);
+
+$label = optional_param('label', false, PARAM_TEXT);
+$graph->title->Set($label);
+
+
+$labels = optional_param('labels', false, PARAM_TEXT);
+$data = optional_param('data', false, PARAM_TEXT);
+
+
+
+$datay=array(62,105,85,50);
+$labelss = array('A','B','C','D');
+
+
+
+var_dump($data);
+var_dump($datay);
+
+
 
 //$theme_class="DefaultTheme";
 //$graph->SetTheme(new $theme_class());
@@ -41,7 +50,7 @@ $graph->SetBox(false);
 //$graph->ygrid->SetColor('gray');
 $graph->ygrid->SetFill(false);
 
-$graph->xaxis->SetTickLabels(array('A','B','C','D'));
+$graph->xaxis->SetTickLabels($labelss);
 $graph->yaxis->HideLine(false);
 $graph->yaxis->HideTicks(false,false);
 
@@ -55,22 +64,13 @@ $graph->Add($b1plot);
 $b1plot->SetColor("white");
 $b1plot->SetFillGradient("#4B0082","white",GRAD_LEFT_REFLECTION);
 $b1plot->SetWidth(45);
-$graph->title->Set($label);
+
 
 // Display the graph
-$graph->Stroke();
+//$graph->Stroke();
 
 
 
 
 
 
-
-
-
-//$img = new img();
-//http_response_code($img->getResponseCode());
-//header('Content-Type: application/json');
-//
-//echo json_encode($data, JSON_PRETTY_PRINT);
-//exit;
