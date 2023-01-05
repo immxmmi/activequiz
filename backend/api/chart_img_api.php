1948c8a83c84dd60dcf500396f0b88cc3e98bf51
Momo <76620405+immxmmi@users.noreply.github.com>
2023-01-05 10:10:58 +0100

    TEST

--- backend/api/chart_img_api.php
+++ backend/api/chart_img_api.php
@@ -17,41 +17,79 @@ global $DB;
 
 // PARAMETER
 //$rowdata = optional_param('c', false, PARAM_TEXT);
+$height = optional_param('height', false, PARAM_TEXT);
+$weight = optional_param('weight', false, PARAM_TEXT);
 $type = optional_param('type', false, PARAM_TEXT);
 $labels = optional_param('labels', false, PARAM_TEXT);
 $label = optional_param('label', false, PARAM_TEXT);
 $datas = optional_param('data', false, PARAM_TEXT);
 
+if(!$height){
+    $height = 250;
+}
+if(!weight){
+    $weight = 400;
+}
+//Set the data
 
-$data = array(10,6,16,23,11,9,5);
+$series1=array(10,60,30,70,25,67,10);
 
-//Declare the graph object
+$series2=array(34,89,56,12,59,70,23);
 
-$graph = new Graph(400,250);
+
+
+//Declare object to draw the chart
+
+$graph = new Graph(500,300);
 
 //Clear all
 
 $graph->ClearTheme();
 
-//Set the scale
+//Set some setting for the chart
 
-$graph->SetScale('textlin');
+$graph->SetScale("textlin");
 
-//Set the linear plot
+$graph->SetShadow();
 
-$linept=new LinePlot($data);
+$graph->img->SetMargin(80,30,20,50);
 
-//Set the line color
 
-$linept->SetColor('green');
 
-//Add the plot to create the chart
+//Create object and colors for the bar plots
 
-$graph->Add($linept);
+$b1plot = new BarPlot($series1);
 
-//Display the chart
+$b1plot->SetFillColor("blue");
+
+$b2plot = new BarPlot($series2);
+
+$b2plot->SetFillColor("red");
+
+
+
+//Create object for the grouped bar plot
+
+$gbplot = new AccBarPlot(array($b1plot, $b2plot));
+
+
+
+//Add the plot for the chart
+
+$graph->Add($gbplot);
+
+//Set title of the chart, x-axis and y-axis
+
+$graph->title->Set("Accumulated Bar Chart");
+
+$graph->xaxis->title->Set("Series-1");
+
+$graph->yaxis->title->Set("Series-2");
+
+//Display the graph
 
 $graph->Stroke();
+
 
 
 
