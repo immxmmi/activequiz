19234e8db0630207993b5444086b312f9e9e99fa
Momo <76620405+immxmmi@users.noreply.github.com>
2023-01-06 02:04:43 +0100

    TEST

--- backend/api/chart_img_api.php
+++ backend/api/chart_img_api.php
@@ -22,8 +22,7 @@ $img_build = new chart_img_builder($height,$weight,$label,$data, $labels);
 
 
 // Create the graph
-//$graph = new Graph($img_build->getWeight(),$img_build->getHeight(),'auto');
-$graph = new Graph(300,200);
+$graph = new Graph($img_build->getWeight(),$img_build->getHeight(),'auto');
 $graph->SetScale('intlin');
 
 // Add a drop shadow
