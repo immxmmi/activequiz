2432d3773f95f435106453aad987737665f87e9d
Momo <76620405+immxmmi@users.noreply.github.com>
2023-01-08 18:58:13 +0100

    TEST

--- js/pdf_generator.js
+++ js/pdf_generator.js
@@ -197,7 +197,7 @@ async function createPdf(sessionID, sessionName) {
             let answers;
 
             // CHART API
-            getChartDataBySessionID(sessionID,2).then((chartData) => {
+            getChartDataBySessionID(sessionID,1).then((chartData) => {
                 // Chart data
 
                 const chartType = chartData.data.charttype;
