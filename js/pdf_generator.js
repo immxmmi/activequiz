a785a2f2de889b94e8c2d0c4db32fcbba23bc6e2
Momo <76620405+immxmmi@users.noreply.github.com>
2023-01-08 14:43:28 +0100

    TEST

--- js/pdf_generator.js
+++ js/pdf_generator.js
@@ -1,40 +1,6 @@
 // QUIZ DATA
 const {PDFDocument, StandardFonts, rgb} = PDFLib
-
-// Generate Chart By Parameter
-function generateChartBySessionAndSlot(sessionid, type, slot) {
-    var url = '/mod/activequiz/backend/api/chart_api.php?sessionid=' + sessionid + '&type=' + type + '&slot=' + slot;
-    return fetch(url).then((response) => response.json());
-}
-
-// Generate Chart By Parameter
-async function getQuizDataBySession(sessionid) {
-    var url = '/mod/activequiz/backend/api/quiz_api.php?sessionid=' + sessionid;
-    return fetch(url).then((response) => response.json());
-}
-
-// TODO
-async function getChartDataBySessionID(sessionID) {
-    // for (let slot = 1; slot < slots; slot++) {
-    return generateChartBySessionAndSlot(sessionID, 'bar', 1);
-    //  generateChartBySessionAndSlot(sessionID, 'pie', slot);
-    //   generateChartBySessionAndSlot(sessionID, 'doughnut', slot);
-    // }
-}
-
-
-
-
-function createChartLink(chartType, title, labels, data, question, xlabel, ylabel) {
-    let labelsStr = labels.map(x => "'" + x + "'").toString();
-    const height = 250;
-    const weight = 350;
-   // const url = `https://quickchart.io/chart?width=500&height=300&c={type:'${chartType}',data:{labels:[${labelsStr}], datasets:[{label:'${label}',data:[${data}]}]}}`;
-    var url = `./backend/api/chart_img_api.php?type=${chartType}&height=${height}&weight=${weight}&title=${title}&labels=${labelsStr}&data=${data}&xlabel=${xlabel}&ylabel=${ylabel}`;
-    return encodeURI(url);
-}
-
-
+import "./chart_img_generator";
 
 // TODO
 async function buildPdf(sessionName,chartType, label, labels, data, rightAnswer, question, answers) {
@@ -177,7 +143,6 @@ async function buildPdf(sessionName,chartType, label, labels, data, rightAnswer,
     // Download
     download(pdfBytes, sessionName + time.toString(), "application/pdf");
 }
-
 
 async function createPdf(sessionID, sessionName) {
     if (sessionID == null) {
