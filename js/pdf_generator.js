489b82f5634c86e9888c10d08c5b3f78487ee9e5
Momo <76620405+immxmmi@users.noreply.github.com>
2023-01-08 20:17:39 +0100

    TEST

--- js/pdf_generator.js
+++ js/pdf_generator.js
@@ -67,9 +67,15 @@ async function buildPdf(currentQuizList) {
    //
     var sessionName = currentQuizList.at(0).sessionName;
     var chartType = currentQuizList.at(0).chartType;
+    var label = currentQuizList.at(0).label;
+    var labels = currentQuizList.at(0).labels;
+    var data = currentQuizList.at(0).data;
+    var rightAnswer = currentQuizList.at(0).rightAnswer;
 
     console.log(sessionName);
     console.log(chartType);
+    console.log(data);
+    console.log(rightAnswer);
 
 
     /*
