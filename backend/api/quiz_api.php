e44dc4b65ef560ec0091418358855be57c979b15
Momo <76620405+immxmmi@users.noreply.github.com>
2023-01-08 16:15:07 +0100

    TEST

--- backend/api/quiz_api.php
+++ backend/api/quiz_api.php
@@ -48,7 +48,7 @@ foreach ($quizdata as $qd) {
 $data = $quiz_build->build_quiz_data($qu, $aw, $right, $slots);
 
 echo "<pre>";
-print_r(new question_data(1));
+print_r(new question_data(2));
 echo "</pre>";
 
 
