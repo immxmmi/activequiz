cfd3a68e1ee2086194f14165ae918c55a24fc1bf
Momo <76620405+immxmmi@users.noreply.github.com>
2023-01-08 14:11:37 +0100

    TEST

--- classes/output/report_overview_renderer.php
+++ classes/output/report_overview_renderer.php
@@ -65,6 +65,7 @@ class report_overview_renderer extends \plugin_renderer_base
         }
 
         $sessionselect = new \single_select($sessionselecturl, 'sessionid', $sessionoptions, $selectedid);
+        $sessionName = $selectsession->get_session()->name;
 
 
 
@@ -79,7 +80,7 @@ class report_overview_renderer extends \plugin_renderer_base
         $selectsession .= \html_writer::tag('script', '', array('src' => 'https://cdn.jsdelivr.net/npm/chart.js@2.9.3'));
 
         //PDF PRINTER
-        $sessionName = $session->get_session()->name;
+        console.log();
         $selectsession .= \html_writer::tag('button', 'PDF Download', array('id' => 'printPfd', 'type' => 'submit', 'class' => 'btn btn-info', 'OnClick' => 'createPdf('.$selectedid.', "'.$sessionName.'")'));
         $output .= $selectsession;
 
