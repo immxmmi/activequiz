1fed2f9ca32142adcda7257c3bc6b4cc7162e5f0
Momo <76620405+immxmmi@users.noreply.github.com>
2023-01-08 15:54:52 +0100

    TEST

--- backend/api/quiz_api.php
+++ backend/api/quiz_api.php
@@ -26,7 +26,6 @@ $activequiz_attempt = new activequiz_attempt($sessionid);
 $allquestionengids = $activequiz_attempt->getAllQuestionengids();
 #######################################################
 echo '<pre>';
-echo 'TESSSSSSSSSSSSSSST';
 print_r($activequiz_attempt->getActiveAttemps()[0]->getQubalayout());
 echo '</pre>';
 
